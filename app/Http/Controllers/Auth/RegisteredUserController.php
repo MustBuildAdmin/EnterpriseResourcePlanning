4d834155f2909ed150efdbcb9d0c2897859fc66f
gomagomathy <gomagomathy17223@gmail.com>
2023-03-26 19:01:05 +0530

    Registeration Password Aunthentication

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -89,7 +89,7 @@ class RegisteredUserController extends Controller
             $status = Password::sendResetLink(
                 $request->only('email')
             );
-    
+            event(new Registered($user));
             return $status == Password::RESET_LINK_SENT ? back()->with('status', __($status)) : back()->withInput($request->only('email'))->withErrors(['email' => __($status)]);
         }
        
