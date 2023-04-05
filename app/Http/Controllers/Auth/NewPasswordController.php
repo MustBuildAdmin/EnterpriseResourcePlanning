4925860fc4ee6a83a2c5de102bf6f551019e74ba
gomagomathy <gomagomathy17223@gmail.com>
2023-04-05 07:59:52 +0530

    Email Tempaltes & Registration

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -9,12 +9,15 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Str;
 use Illuminate\Validation\Rules;
-
+use App\Models\User;
+use DB;
 use App\Models\ExperienceCertificate;
 use App\Models\GenerateOfferLetter;
 use App\Models\JoiningLetter;
 use  App\Models\Utility;
-
+use Spatie\Permission\Models\Role;
+use App\Models\NOC;
+use App\Models\Plan;
 class NewPasswordController extends Controller
 {
     /**
@@ -28,6 +31,10 @@ class NewPasswordController extends Controller
         return view('auth.passwords.reset', ['request' => $request]);
     }
 
+    public function setpassword(Request $request)
+    {
+        return view('auth.passwords.setpassword', ['request' => $request]);
+    }
     /**
      * Handle an incoming new password request.
      *
@@ -104,5 +111,60 @@ class NewPasswordController extends Controller
                     ? redirect()->route('login')->with('status', __($status))
                     : back()->withInput($request->only('email'))
                             ->withErrors(['email' => __($status)]);
+    }
+    public function storereset(Request $request)
+    {
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        // Here we will attempt to reset the user's password. If it is successful we
+        // will update the password on an actual user model and persist it to the
+        // database. Otherwise we will parse the error and return the response.
+        $password=Hash::make($request->password);
+        $update_user_password=User::where(['email'=>$request->email])->update(['password'=>$password]);
+        $user=User::where(['email'=>$request->email])->first();
+        if($user){
+            $insert_array=array(
+                'name'=>'company_name',
+                'value'=>$user->company_name,
+                'created_by'=>$user->id,
+            );
+            $data =DB::table('settings')->insert($insert_array);
+
+            $insert2=array(
+                'name'=>'company_type',
+                'value'=>$user->company_type,
+                'created_by'=>$user->id,
+            );
+            $data =DB::table('settings')->insert($insert2);
+            
+
+            $role_r = Role::findByName('company');
+            $user->assignRole($role_r);
+            $user->userDefaultDataRegister($user->id);
+            $user->userWarehouseRegister($user->id);
+            Utility::chartOfAccountTypeData($user->id);
+            Utility::chartOfAccountData1($user->id);
+            Utility::pipeline_lead_deal_Stage($user->id);
+            Utility::project_task_stages($user->id);
+            Utility::labels($user->id);
+            Utility::sources($user->id);
+            Utility::jobStage($user->id);
+            GenerateOfferLetter::defaultOfferLetterRegister($user->id);
+            ExperienceCertificate::defaultExpCertificatRegister($user->id);
+            JoiningLetter::defaultJoiningLetterRegister($user->id);
+            NOC::defaultNocCertificateRegister($user->id);
+
+            event(new Registered($user));
+
+            Auth::login($user);
+
+            return \Redirect::to('paymentPage');
+
+                event(new PasswordReset($user));
+            }
     }
 }
--- resources/views/new_layouts/header.blade.php
+++ resources/views/new_layouts/header.blade.php
@@ -61,8 +61,16 @@
 				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-menu" aria-controls="navbar-menu" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
 				<h1 class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
                     <a href=".">
-                        <img src="./static/logo.svg" width="110" height="32" alt="Must BuildApp"
+                        
+							@if($mode_setting['cust_darklayout'] && $mode_setting['cust_darklayout'] == 'on' )
+								<img src="{{ $logo . '/' . (isset($company_logos) && !empty($company_logos) ? $company_logos : 'logo-dark.png') }}"
+									alt="{{ config('app.name', 'ERPGo-SaaS') }}"  width="110" height="32" alt="Must BuildApp"
                             class="navbar-brand-image">
+							@else
+								<img src="{{ $logo . '/' . (isset($company_logo) && !empty($company_logo) ? $company_logo : 'logo-dark.png') }}"
+									alt="{{ config('app.name', 'ERPGo-SaaS') }}"  width="110" height="32" alt="Must BuildApp"
+                            class="navbar-brand-image">
+							@endif
                     </a>
                 </h1>
 				<div class="navbar-nav flex-row order-md-last">
