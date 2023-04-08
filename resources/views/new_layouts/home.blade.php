e096d26cdcc84cf657dddf04b9f9d9578ad8bfd8
MustBuildAdmin <admin@mustbuildapp.com>
2023-04-09 02:13:59 +0530

    changes in the home screen

--- resources/views/new_layouts/home.blade.php
+++ resources/views/new_layouts/home.blade.php
@@ -41,6 +41,7 @@
 								<div class="card-body">
 									<div class="row">
 										<div class="col-auto">
+                                            <span class="bg-primary text-white avatar">
 											<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-users" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
 												<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
 												<path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path>
@@ -48,6 +49,7 @@
 												<path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
 												<path d="M21 21v-2a4 4 0 0 0 -3 -3.85"></path>
 											</svg>
+                                            </span>
 										</div>
 										<div class="col">
 											<div class="font-weight-medium">{{__('Users')}}</div>
@@ -63,12 +65,14 @@
 								<div class="card-body">
 									<div class="row">
 										<div class="col-auto">
+                                            <span class="bg-primary text-white avatar">
 											<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-circle" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
 												<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
 												<path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
 												<path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
 												<path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855"> </path>
 											</svg>
+                                            </span>
 										</div>
 										<div class="col">
 											<div class="font-weight-medium">{{__('Roles')}}</div>
