d4aad304b2e06c52ed71c97eba72d12d68e4b597
unknown <vijay@mustbuildapp.com>
2023-04-12 20:13:59 +0530

    changes in code

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -84,7 +84,7 @@ $logo=\App\Models\Utility::get_file('uploads/logo');
 
             <div class="mb-3">
                 <label for="name" class="form-label">{{__('Name')}}</label>
-                <input id="name" type="text" min="3" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
                 @error('name')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -132,7 +132,7 @@ $logo=\App\Models\Utility::get_file('uploads/logo');
             </div> -->
             <div class="mb-3">
                 <label for="company_name" class="form-label">{{__('Company Name')}}</label>
-                <input id="company_name"  min="3" type="text" data-indicator="company_name" class="form-control pwstrength @error('company_name') is-invalid @enderror" name="company_name" required autocomplete="new-password">
+                <input id="company_name" type="text" data-indicator="company_name" class="form-control pwstrength @error('company_name') is-invalid @enderror" name="company_name" required autocomplete="new-password">
                 @error('company_name')
                 <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
