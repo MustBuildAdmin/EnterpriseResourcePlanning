7243e6090be8e1e58fde07cbad85d4c5b39ac888
sureshmagesh14 <thandapanimagi@gmail.com>
2023-04-13 18:59:13 +0530

    report

--- resources/views/construction_project/report.blade.php
+++ resources/views/construction_project/report.blade.php
@@ -93,26 +93,6 @@
                                 {{-- // ################################ --}}
                                 <div class="col-12">
                                     <div class="card">
-                                      <div class="card-header">
-                                        <h3 class="card-title">Invoices</h3>
-                                      </div>
-                                      <div class="card-body border-bottom py-3">
-                                        <div class="d-flex">
-                                          <div class="text-muted">
-                                            Show
-                                            <div class="mx-2 d-inline-block">
-                                              <input type="text" class="form-control form-control-sm" value="8" size="3" aria-label="Invoices count">
-                                            </div>
-                                            entries
-                                          </div>
-                                          <div class="ms-auto text-muted">
-                                            Search:
-                                            <div class="ms-2 d-inline-block">
-                                              <input type="text" class="form-control form-control-sm" aria-label="Search invoice">
-                                            </div>
-                                          </div>
-                                        </div>
-                                      </div>
                                       <div class="table-responsive">
                                         <table class="table card-table table-vcenter text-nowrap datatable">
                                           <thead>
@@ -195,7 +175,7 @@
                                         </tbody>
                                       
                                     </div>
-                                    {{$projects->links() }}
+                                    
                         </div>
                     </div>
                 </div>
