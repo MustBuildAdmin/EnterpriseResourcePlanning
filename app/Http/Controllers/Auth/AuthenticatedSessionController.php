a30e3afefaa5464649edd4d84dc7577b7c2e9b3a
unknown <vijay@mustbuildapp.com>
2023-04-21 20:18:06 +0530

    chnages in code

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -87,9 +87,15 @@ class AuthenticatedSessionController extends Controller
 
         $update_lang=array('lang'=>$request->lang);
 
-        if($check_email->lang!=null){
+        if($check_email->lang!=null && $request->lang!='en'){
 
             DB::table('users')->where('id',Auth::id())->where('email',$request->email)->update($update_lang);
+
+        }else if($check_email->lang==null){
+
+            DB::table('users')->where('id',Auth::id())->where('email',$request->email)->update($update_lang);
+            
+        }else{
 
         }
 
