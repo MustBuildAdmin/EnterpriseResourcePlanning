a86a6f39ef1555a2ef7264115daf936762cba90c
MustBuildAdmin <admin@mustbuildapp.com>
2023-04-25 23:29:20 +0530

    ui corrections in construction template

--- resources/views/construction_project/side-menu.blade.php
+++ resources/views/construction_project/side-menu.blade.php
@@ -24,7 +24,7 @@
                         {{-- <h4 class="subheader mt-4">Construction Setting</h4>
                         <div class="list-group list-group-transparent">
                             <a href="#" class="list-group-item list-group-item-action">Project Task Stages</a>
-                        </div> --}}
+                        </div>
                     </div>
                 </div> --}}
                 <style>
@@ -34,7 +34,7 @@
                         margin: 18px 0;
                         color: #6c58bF;
                     }
-                
+
                     .pagination ul {
                         display: inline-block;
                         *display: inline;
@@ -50,12 +50,12 @@
                         -moz-box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05);
                         box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05);
                     }
-                
+
                     .pagination li {
                         display: inline;
                         color: #6c58bF;
                     }
-                
+
                     .pagination a {
                         float: left;
                         padding: 0 14px;
@@ -65,24 +65,24 @@
                         border: 1px solid #ddd;
                         border-left-width: 0;
                     }
-                
+
                     .pagination a:hover,
                     .pagination .active a {
                         background-color: var(--tblr-pagination-active-bg);
                         color: #ffffff;
                     }
-                
+
                     .pagination a:focus {
                         background-color: #ffffff;
                         color: #ffffff;
                     }
-                
-                
+
+
                     .pagination .active a {
                         color: #ffffff;
                         cursor: default;
                     }
-                
+
                     .pagination .disabled span,
                     .pagination .disabled a,
                     .pagination .disabled a:hover {
@@ -90,28 +90,28 @@
                         background-color: transparent;
                         cursor: default;
                     }
-                
+
                     .pagination li:first-child a {
                         border-left-width: 1px;
                         -webkit-border-radius: 3px 0 0 3px;
                         -moz-border-radius: 3px 0 0 3px;
                         border-radius: 3px 0 0 3px;
                     }
-                
+
                     .pagination li:last-child a {
                         -webkit-border-radius: 0 3px 3px 0;
                         -moz-border-radius: 0 3px 3px 0;
                         border-radius: 0 3px 3px 0;
                     }
-                
+
                     .pagination-centered {
                         text-align: center;
                     }
-                
+
                     .pagination-right {
                         text-align: right;
                     }
-                
+
                     .pager {
                         margin-left: 0;
                         margin-bottom: 18px;
@@ -120,22 +120,22 @@
                         color: #6c58bF;
                         *zoom: 1;
                     }
-                
+
                     .pager:before,
                     .pager:after {
                         display: table;
                         content: "";
                     }
-                
+
                     .pager:after {
                         clear: both;
                     }
-                
+
                     .pager li {
                         display: inline;
                         color: #6c58bF;
                     }
-                
+
                     .pager a {
                         display: inline-block;
                         padding: 5px 14px;
@@ -146,32 +146,32 @@
                         -moz-border-radius: 15px;
                         border-radius: 15px;
                     }
-                
+
                     .pager a:hover {
                         text-decoration: none;
                         background-color: #f5f5f5;
                     }
-                
+
                     .pager .next a {
                         float: right;
                     }
-                
+
                     .pager .previous a {
                         float: left;
                     }
-                
+
                     .pager .disabled a,
                     .pager .disabled a:hover {
                         color: #999999;
                     }
-                
+
                     .dataTables_wrapper .dataTables_paginate {
                         float: right;
                         text-align: right;
                         padding-top: 0.25em;
                     }
                 </style>
-                
+
                 <div class="wrapper">
                     <!-- Sidebar  -->
                     <nav id="sidebar">
@@ -181,7 +181,7 @@
                                     <a href="{{route('filter.project.view')}}"><span class="icon"><i class="ti ti-dashboard"></i></span><span
                                             class="list">{{ __('Productivity') }}</span></a>
                                 </li>
-                
+
                                 <li class="">
                                     <a href="#"><span
                                             class="icon"><i class="ti ti-users"></i>
@@ -192,7 +192,7 @@
                                     <a href="{{ route('taskBoard.view',['list']) }}"> <span class="icon"><i class="ti ti-calendar-stats"></i></span><span
                                             class="list">{{ __('Task') }}</span></a>
                                 </li>
-                
+
                                 <li class="{{ (request()->is('calendar_new*') ? 'active' : '')}}">
                                     <a href="{{ route('task.newcalendar',['all']) }}"><span class="icon"><i class="ti ti-calendar-stats"></i></span><span
                                             class="list">{{ __('Task Calender') }}</span></a>
@@ -204,7 +204,7 @@
                             </ul>
                         </div>
                     </nav>
-                
+
                       <!-- Page Content  -->
     <div id="content" class="main">
         <div class="collapseToggle">
@@ -213,8 +213,8 @@
         @isset($hrm_header)
             <h2 class="mb-4">{{ __($hrm_header) }}</h2>
         @endisset
-                
-                
+
+
 <script type="text/javascript">
     $('.collapseToggle').on('click', function() {
             $(".sidebar").toggleClass('sidebar--Collapse');
@@ -222,5 +222,3 @@
             $('#toggleIcon').toggleClass('rotate');
     });
 </script>
-                {{-- @include('new_layouts.footer') --}}
-                
