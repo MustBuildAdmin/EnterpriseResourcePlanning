075a33f0dbc47b29a204bb4f85341ae93235f771
unknown <vijay@mustbuildapp.com>
2023-04-30 14:38:33 +0530

    crm chnages

---
+++ resources/views/crm/form_builder/index.blade.php
@@ -0,0 +1,14 @@
+@include('new_layouts.header')
+<div class="page-wrapper"> 
+    @include('crm.side-menu', ['hrm_header' => 'Manage Form Builder'])
+	<div class="col d-flex flex-column">
+		<div class="card-body">
+			<div class="col-xl-12 col-lg-12 col-sm-12">
+				<div class="card">
+					<div class="card-body">
+						<h6 class="text-center mb-0">{{ __('content') }}</h6> </div>
+				</div>
+			</div>
+		</div>
+	</div> 
+    {{-- @include('new_layouts.footer') --}}
