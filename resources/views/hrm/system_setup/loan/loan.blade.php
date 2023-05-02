299dec4523feef95b218d0b1cf4ff325681d2de9
mohan <mohank@yelagiriinfotech.com>
2023-05-02 21:42:31 +0530

    UI Changes

--- resources/views/hrm/system_setup/loan/loan.blade.php
+++ resources/views/hrm/system_setup/loan/loan.blade.php
@@ -1,11 +1,25 @@
 @include('new_layouts.header')
-@include('hrm.hrm_main',['hrm_header' => 'Loan Option'])
+@include('hrm.hrm_main')
+
+
+
+
+<div class="row">
+  <div class="col-md-6">
+     <h2>Loan Option</h2>
+  </div>
+  <div class="col-md-6 float-end floatrght">
 
     @can('create loan option')
-        <a href="#" data-url="{{ route('loanoption.create') }}" data-ajax-popup="true" data-title="{{__('Create New Loan Option')}}" data-bs-toggle="tooltip" title="{{__('Create')}}"  class="mb-3 btn btn-sm btn-primary">
+        <a class="floatrght mb-3 btn btn-sm btn-primary" href="#" data-url="{{ route('loanoption.create') }}" data-ajax-popup="true" data-title="{{__('Create New Loan Option')}}" data-bs-toggle="tooltip" title="{{__('Create')}}">
             {{__('Create')}} &nbsp;<i class="ti ti-plus"></i>
         </a>
     @endcan
+
+  </div>
+</div>
+
+
     <div class="table-responsive">
         <table class="table card-table table-vcenter text-nowrap datatable">
             <thead>
