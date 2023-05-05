1f02ef875874598f72be16debe05943dc4d95847
MustBuildAdmin <109482580+MustBuildAdmin@users.noreply.github.com>
2023-05-05 19:41:05 +0530

    Emailtemplates (#108)
    * Leave
    
    * Mydetails
    
    ---------
    
    Co-authored-by: gomagomathy <gomagomathy17223@gmail.com>

--- resources/views/new_layouts/home.blade.php
+++ resources/views/new_layouts/home.blade.php
@@ -215,6 +215,22 @@
 								</div>
 							</a>
 						</div>
+						<div class="col-md-6 col-xl-2">
+							<a class="card card-link" href="{{route('mark-attendance')}}" >
+								<div class="card-body">
+									<div class="row">
+										<div class="col-auto">
+                                            <span class="bg-success text-white avatar">
+											<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16"> <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/> <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/> </svg>
+                                            </span>
+										</div>
+										<div class="col">
+											<div class="font-weight-medium mt-2">{{__('Mark Attendance')}}</div>
+										</div>
+									</div>
+								</div>
+							</a>
+						</div>
 						@endif
 						@if((\Auth::user()->type != 'super admin'))
                         @if( Gate::check('manage company plan') || Gate::check('manage order') || Gate::check('manage company settings'))
