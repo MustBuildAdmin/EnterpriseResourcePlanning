9d03ecb3ac7e2b190acae5070d3b77fee9748ad0
unknown <balamurugan@mustbuildapp.com>
2023-05-16 20:47:57 +0530

    branch hrm

--- resources/views/hrm/system_setup/branch/branch_create.blade.php
+++ resources/views/hrm/system_setup/branch/branch_create.blade.php
@@ -6,6 +6,7 @@
             <div class="form-group">
                 {{Form::label('name',__('Name*'),['class'=>'form-label'])}}
                 {{Form::text('name',null,array('class'=>'form-control get_name','required' => 'required','placeholder'=>__('Enter Branch Name')))}}
+                <br>
                 <span class="invalid-name show_duplicate_error" role="alert" style="display: none;">
                     <strong class="text-danger">Branch Name Already Exist!</strong>
                 </span> 
@@ -20,23 +21,26 @@
 </div>
 <div class="modal-footer">
     <input type="button" value="{{__('Cancel')}}" class="btn btn-light" data-bs-dismiss="modal">
-    <input type="button" value="{{__('Create')}}" class="btn btn-primary submit_button">
+    <input type="submit" value="{{__('Create')}}" class="btn btn-primary submit_button">
 </div>
 {{Form::close()}}
 
 <script>
     $(document).ready(function(){
-        $(".submit_button").click(function(){
+        $(".get_name").keyup(function(){
+            $(".submit_button").prop('disabled',false);
             $(".show_duplicate_error").css('display','none');
             $.ajax({
                 url : '{{ route("checkDuplicateRS_HRM") }}',
                 type : 'GET',
                 data : { 'get_name' : $(".get_name").val(),'form_name' : "Branch" },
-                success : function(data) {              
+                success : function(data) {
                     if(data == 1){
-                        $(".form_submit").submit();
+                        $(".submit_button").prop('disabled',false);
+                        $(".show_duplicate_error").css('display','none');
                     }
                     else{
+                        $(".submit_button").prop('disabled',true);
                         $(".show_duplicate_error").css('display','block');
                     }
                 },
