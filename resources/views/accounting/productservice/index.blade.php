5458dab8573985d4bee9b7dbb538868a2ce88b54
balaji <1988.balaji@gmail.com>
2023-05-20 17:53:51 +0530

    Ui changes accounting

--- resources/views/accounting/productservice/index.blade.php
+++ resources/views/accounting/productservice/index.blade.php
@@ -2,27 +2,29 @@
 @include('new_layouts.header')
 @include('accounting.side-menu')
 
-<div class="row">
-    <div class="col-md-6">
-       <h2>{{__('Manage Product & Services')}}</h2>
-    </div>
 
-    <div class="float-end">
-        {{--        <a class="btn btn-sm btn-primary" data-bs-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1" data-bs-toggle="tooltip" title="{{__('Filter')}}">--}}
-        {{--            <i class="ti ti-filter"></i>--}}
-        {{--        </a>--}}
-        <a href="#" data-size="md"  data-bs-toggle="tooltip" title="{{__('Import')}}" data-url="{{ route('productservice.file.import') }}" data-ajax-popup="true" data-title="{{__('Import product CSV file')}}" class="btn btn-sm btn-primary">
+<div class="row">
+  <div class="col-md-6">
+      <h2>{{__('Manage Product & Services')}}</h2>
+  </div>
+  <div class="col-md-6 float-end floatrght">
+
+        <a href="#" data-size="md"  data-bs-toggle="tooltip" title="{{__('Import')}}" data-url="{{ route('productservice.file.import') }}" data-ajax-popup="true" data-title="{{__('Import product CSV file')}}" class="floatrght gapbtn btn btn-sm btn-primary">
             <i class="ti ti-file-import"></i>
         </a>
-        <a href="{{route('productservice.export')}}" data-bs-toggle="tooltip" title="{{__('Export')}}" class="btn btn-sm btn-primary">
+        <a href="{{route('productservice.export')}}" data-bs-toggle="tooltip" title="{{__('Export')}}" class="gapbtn floatrght btn btn-sm btn-primary">
             <i class="ti ti-file-export"></i>
         </a>
 
-        <a href="#" data-size="lg" data-url="{{ route('productservice.create') }}" data-ajax-popup="true" data-bs-toggle="tooltip" title="{{__('Create New Product')}}" class="btn btn-sm btn-primary">
+        <a href="#" data-size="lg" data-url="{{ route('productservice.create') }}" data-ajax-popup="true" data-bs-toggle="tooltip" title="{{__('Create New Product')}}" class="gapbtn floatrght btn btn-sm btn-primary">
             <i class="ti ti-plus"></i>
         </a>
 
-    </div>
+  </div>
+</div>
+
+
+<div class="row">
 
     <div class="row">
         <div class="col-sm-12">
@@ -61,7 +63,7 @@
             <div class="card">
                 <div class="card-body table-border-style">
                     <div class="table-responsive">
-                        <table class="table datatable">
+                        <table class="table datatable bill">
                             <thead>
                             <tr>
                                 <th>{{__('Name')}}</th>
@@ -73,7 +75,7 @@
                                 <th>{{__('Unit')}}</th>
                                 <th>{{__('Quantity')}}</th>
                                 <th>{{__('Type')}}</th>
-                                <th>{{__('Action')}}</th>
+                                <th style="width:15%">{{__('Action')}}</th>
                             </tr>
                             </thead>
                             <tbody>
@@ -102,23 +104,23 @@
                                     <td>{{ $productService->type }}</td>
 
                                     @if(Gate::check('edit product & service') || Gate::check('delete product & service'))
-                                        <td class="Action">
-                                            <div class="action-btn bg-warning ms-2">
-                                                <a href="#" class="mx-3 btn btn-sm align-items-center" data-url="{{ route('productservice.detail',$productService->id) }}"
+                                        <td>
+                                            <div class="ms-2">
+                                                <a href="#" class="mx-3 btn btn-sm align-items-center backgroundnone" data-url="{{ route('productservice.detail',$productService->id) }}"
                                                    data-ajax-popup="true" data-bs-toggle="tooltip" title="{{__('Warehouse Details')}}" data-title="{{__('Warehouse Details')}}">
                                                     <i class="ti ti-eye text-white"></i>
                                                 </a>
                                             </div>
 
                                             @can('edit product & service')
-                                                <div class="action-btn bg-info ms-2">
-                                                    <a href="#" class="mx-3 btn btn-sm  align-items-center" data-url="{{ route('productservice.edit',$productService->id) }}" data-ajax-popup="true"  data-size="lg " data-bs-toggle="tooltip" title="{{__('Edit')}}"  data-title="{{__('Product Edit')}}">
+                                                <div class="ms-2">
+                                                    <a href="#" class="mx-3 btn btn-sm  align-items-center backgroundnone" data-url="{{ route('productservice.edit',$productService->id) }}" data-ajax-popup="true"  data-size="lg " data-bs-toggle="tooltip" title="{{__('Edit')}}"  data-title="{{__('Product Edit')}}">
                                                         <i class="ti ti-pencil text-white"></i>
                                                     </a>
                                                 </div>
                                             @endcan
                                             @can('delete product & service')
-                                                <div class="action-btn bg-danger ms-2">
+                                                <div class="ms-2">
                                                     {!! Form::open(['method' => 'DELETE', 'route' => ['productservice.destroy', $productService->id],'id'=>'delete-form-'.$productService->id]) !!}
                                                     <a href="#" class="mx-3 btn btn-sm  align-items-center bs-pass-para" data-bs-toggle="tooltip" title="{{__('Delete')}}" ><i class="ti ti-trash text-white"></i></a>
                                                     {!! Form::close() !!}
--- resources/views/accounting/productstock/index.blade.php
+++ resources/views/accounting/productstock/index.blade.php
@@ -1,5 +1,9 @@
 @include('new_layouts.header')
 @include('accounting.side-menu')
+
+
+
+
 <div class="row">
     <div class="col-md-6">
        <h2>{{__('Manage Product Stock')}}</h2>
@@ -26,7 +30,7 @@
                                     <td>{{ $productService->quantity }}</td>
 
                                     <td class="Action">
-                                        <div class="action-btn bg-info ms-2">
+                                        <div class="action-btn ms-2">
                                             <a data-size="md" href="#" class="mx-3 btn btn-sm d-inline-flex align-items-center" data-url="{{ route('productstock.edit', $productService->id) }}" data-ajax-popup="true"  data-size="xl" data-bs-toggle="tooltip" title="{{__('Update Quantity')}}">
                                                 <i class="ti ti-plus text-white"></i>
                                             </a>
