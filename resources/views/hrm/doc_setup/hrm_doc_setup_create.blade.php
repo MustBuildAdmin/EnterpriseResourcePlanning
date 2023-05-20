4179cf981f21dfaa7e4476c71cd06b51adcb0a3d
unknown <balamurugan@mustbuildapp.com>
2023-05-20 19:21:25 +0530

    hrm issue

--- resources/views/hrm/doc_setup/hrm_doc_setup_create.blade.php
+++ resources/views/hrm/doc_setup/hrm_doc_setup_create.blade.php
@@ -4,7 +4,11 @@
         <div class="col-md-6">
             <div class="form-group">
                 {{Form::label('name',__('Name*'),['class'=>'form-label'])}}
-                {{Form::text('name',null,array('class'=>'form-control','required'=>'required'))}}
+                {{Form::text('name',null,array('class'=>'form-control get_name','required'=>'required'))}}
+                <br>
+                <span class="invalid-name show_duplicate_error" role="alert" style="display: none;">
+                    <strong class="text-danger">Name Already Exist!</strong>
+                </span>
             </div>
         </div>
 
@@ -35,11 +39,36 @@
 </div>
 <div class="modal-footer">
     <input type="button" value="{{__('Cancel')}}" class="btn btn-light" data-bs-dismiss="modal">
-    <input type="submit" value="{{__('Create')}}" class="btn  btn-primary">
+    <input type="submit" value="{{__('Create')}}" class="btn  btn-primary submit_button">
 </div>
 {{Form::close()}}
 
 <script>
+    $(document).ready(function(){
+        $(document).on("keyup", '.get_name', function () {
+            $(".show_duplicate_error").css('display','none');
+            $.ajax({
+                url : '{{ route("checkDuplicateRS_HRM") }}',
+                type : 'GET',
+                data : { 'get_name' : $(".get_name").val(),'form_name' : "DocumentSetup" },
+                success : function(data) {
+                    if(data == 1){
+                        $(".submit_button").prop('disabled',false);
+                        $(".show_duplicate_error").css('display','none');
+                    }
+                    else{
+                        $(".submit_button").prop('disabled',true);
+                        $(".show_duplicate_error").css('display','block');
+                    }
+                },
+                error : function(request,error)
+                {
+                    alert("Request: "+JSON.stringify(request));
+                }
+            });
+        });
+    });
+
     document.getElementById('document').onchange = function () {
         var src = URL.createObjectURL(this.files[0])
         document.getElementById('image').src = src
