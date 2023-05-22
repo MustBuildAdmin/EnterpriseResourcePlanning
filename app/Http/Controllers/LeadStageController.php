99692720168bf1901cf3a6870244a6fd5494e84f
Amruth <amruthvinayak@gmail.com>
2023-05-22 20:33:04 +0530

    Non duplication issue and UI issue in lead stages (#166)

--- app/Http/Controllers/LeadStageController.php
+++ app/Http/Controllers/LeadStageController.php
@@ -83,7 +83,7 @@ class LeadStageController extends Controller
         {
             $validator = \Validator::make(
                 $request->all(), [
-                                   'name' => 'required|max:20',
+                                   'name' => 'required|max:20|unique:lead_stages',
                                    'pipeline_id' => 'required',
                                ]
             );
