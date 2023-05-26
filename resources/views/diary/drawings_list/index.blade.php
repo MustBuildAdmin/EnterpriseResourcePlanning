222be75ad1f395cb67d3a3513264c4ce385cb252
unknown <vijay@mustbuildapp.com>
2023-05-26 13:42:47 +0530

    diary changes

---
+++ resources/views/diary/drawings_list/index.blade.php
@@ -0,0 +1,13 @@
+@include('new_layouts.header')
+@include('construction_project.side-menu')
+    <div class="row">
+        <div class="col-md-6">
+        <h2>{{__('Drawings List')}}</h2>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <p class="text-center">content</p>
+            </div>
+        </div>
+    </div>
+@include('new_layouts.footer')
--- resources/views/diary/edit_consultant_direction.blade.php
+++ resources/views/diary/edit_consultant_direction.blade.php
@@ -7,7 +7,7 @@
   <div class="row">
     <div class="container">
       <h3 style="text-align: center; font-weight: 800; color: blue"> Consultants Directions Summary</h3>
-      <form class="" action="{{ route('update_consultant_direction') }}" enctype="multipart/form-data" method="POST"> 
+      <form class="" id="edit_data" action="{{ route('update_consultant_direction') }}" enctype="multipart/form-data" method="POST"> 
         @csrf 
         <div class="container">
             <input type="hidden" name="project_id" value="{{$project}}">
@@ -45,31 +45,33 @@
                 </div>
                 <div class="col-md-12 mt-3">
                   <label for="InputRemarks">Attachment</label>
-                  <input name="attach_file_name" required type="file" id="concreteFile" class="form-control" name="file" />
+                  <input name="attach_file_name"  type="file" id="concreteFile" class="form-control"/>
                   <span>{{$consult_dir->attach_file_name}}</span>
                 </div>
               </div>
             </div>
-            <table class="table" id="dynamicAddRemove"> @foreach ($consult_dir_multi as $mutli_data) <tr>
+            <table class="table" id="dynamicAddRemove"> 
+              @foreach ($consult_dir_multi as $key => $mutli_data) 
+              <tr>
                 <td>
                   <h4 style="text-align: center; font-weight: 700"> Initiator Action: </h4>
                   <div class="row mb-5">
                     <div class="col">
                       <div class="form-group">
                         <label for="InputReference">Reference:</label>
-                        <input type="text" value="{{$mutli_data->initiator_reference}}" name="initiator_reference[]" class="form-control" placeholder="Enter your  Reference" />
+                        <input type="text" value="{{$mutli_data['initiator_reference']}}" name="initiator_reference[]" class="form-control" placeholder="Enter your  Reference" />
                       </div>
                     </div>
                     <div class="col">
                       <div class="form-group">
                         <label for="Inputdate">Date:</label>
-                        <input type="date" value="{{$mutli_data->initiator_date}}" name="initiator_date[]" class="form-control" placeholder="Enter your  Date" />
+                        <input type="date" value="{{$mutli_data['initiator_date']}}"  name="initiator_date[]" class="form-control" placeholder="Enter your  Date" />
                       </div>
                     </div>
                     <div class="col-md-12 mt-3">
                       <label for="InputRemarks">Attachment</label>
-                      <input name="initiator_file_name[]" required type="file" id="concreteFile" class="form-control" multiple />
-                      <span>{{$mutli_data->initiator_file_name}}</span>
+                      <input name="initiator_file_name[]"  type="file" id="" class="form-control" multiple />
+                      <span>{{$mutli_data['initiator_file_name']}}</span>
                     </div>
                   </div>
                   <h4 style="text-align: center; font-weight: 700">Replier:</h4>
@@ -77,13 +79,13 @@
                     <div class="col">
                       <div class="form-group">
                         <label for="InputReference">Reference:</label>
-                        <input type="text" value="{{$mutli_data->replier_reference}}" name="replier_reference[]" class="form-control" placeholder="Enter your  Reference" />
+                        <input type="text" value="{{$mutli_data['replier_reference']}}" name="replier_reference[]" class="form-control" placeholder="Enter your  Reference" />
                       </div>
                     </div>
                     <div class="col">
                       <div class="form-group">
                         <label for="Inputdate">Date:</label>
-                        <input type="date" value="{{$mutli_data->replier_date}}" name="replier_date[]" class="form-control" placeholder="Enter your  Date" />
+                        <input type="date" value="{{$mutli_data['replier_date']}}"  name="replier_date[]" class="form-control" placeholder="Enter your  Date" />
                       </div>
                     </div>
                   </div>
@@ -92,31 +94,34 @@
                       <label for="InputRemarks">Status:</label>
                       <select name="replier_status[]" class="form-control" aria-label="Default select example">
                         <option selected disabled>Status</option>
-                        <option value="clear" @if('clear'==$mutli_data->replier_status){ selected }@endif>Clear</option>
-                        <option value="pending" @if('pending'==$mutli_data->replier_status){ selected }@endif>Pending</option>
-                        <option value="withdrawn" @if('withdrawn'==$mutli_data->replier_status){ selected }@endif>Withdrawn</option>
+                        <option value="clear" @if('clear'==$mutli_data['replier_status']){ selected }@endif>Clear</option>
+                        <option value="pending" @if('pending'==$mutli_data['replier_status']){ selected }@endif>Pending</option>
+                        <option value="withdrawn" @if('withdrawn'==$mutli_data['replier_status']){ selected }@endif>Withdrawn</option>
                       </select>
                     </div>
                     <div class="col-12 mt-3">
                       <div class="form-group">
                         <label for="InputRemarks">Remarks/ Notes:</label>
-                        <textarea type="text" class="form-control" name="replier_remark[]" placeholder="Enter your Remarks/ Notes">{{$mutli_data->replier_remark}}</textarea>
+                        <textarea  class="form-control" name="replier_remark[]" placeholder="Enter your Remarks/ Notes">{{ $mutli_data['replier_remark'] }}</textarea>
                       </div>
                     </div>
                     <div class="col-md-12 mt-3">
                       <label for="InputRemarks">Attachment</label>
-                      <input required type="file" name="replier_file_name[]" id="concreteFile" class="form-control"  />
-                      <span>{{$mutli_data->replier_file_name}}</span>
+                      <input  type="file" name="replier_file_name[]" id="" class="form-control"  />
+                      <span>{{ $mutli_data['replier_file_name'] }}</span>
                     </div>
                   </div>
                   @if(!empty($replier_date))
                   <div class="col-md-12 mt-3">
-                    <button type="button" name="add" id="dynamic-ar" class="btn btn-outline-primary">Add Subject</button>
+                    <button type="button" name="add" id="dynamic-ar" class="btn btn-outline-primary">Add More</button>
                   </div>
                   @endif
                 </td>
               </tr>
-            </table> @endforeach <div class="row" style="margin:20px 0px 20px">
+         
+            </table> 
+            @endforeach 
+            <div class="row" style="margin:20px 0px 20px">
               <div class="modal-footer">
                 <input type="button" value="{{__('Cancel')}}" class="btn btn-light" data-bs-dismiss="modal">
                 <input type="submit" value="{{__('Update')}}" class="btn  btn-primary">
@@ -127,3 +132,18 @@
     </div>
   </div>
 </div>
+
+@push('script-page')
+<script type="text/javascript">
+  $(document).ready(function () {
+    var i = 0;
+    $("#edit_data").on('click', '#dynamic-ar', function () {
+        ++i;
+        $("#dynamicAddRemove").append('<tr><td> <h4 style="text-align: center; font-weight: 700">Initiator Action:</h4><div class="row mb-5"><div class="col"><div class="form-group"><label for="InputReference">Reference:</label><input type="text" name="initiator_reference[' + i +']" class="form-control" placeholder="Enter your  Reference"/></div></div><div class="col"><div class="form-group"><label for="Inputdate">Date:</label><input type="date" name="initiator_date[' + i +']" class="form-control" placeholder="Enter your  Date"/></div></div><div class="col-md-12 mt-3"><label for="InputRemarks">Attachment</label><input name="initiator_file_name[' + i +']"  type="file" id="" class="form-control" /></div></div> <h4 style="text-align: center; font-weight: 700">Replier:</h4><div class="row mb-3"><div class="col"><div class="form-group"><label for="InputReference">Reference:</label><input type="text" name="replier_reference[' + i +']" class="form-control" placeholder="Enter your  Reference"/></div></div><div class="col"><div class="form-group"><label for="Inputdate">Date:</label><input type="date" name="replier_date[' + i +']" class="form-control" placeholder="Enter your  Date"/></div></div></div><div class="row mb-5"><div class="col form-group"><label for="InputRemarks">Status:</label><select name="replier_status[' + i +']" class="form-control" aria-label="Default select example"><option selected disabled>Status</option><option value="clear">Clear</option><option value="pending">Pending</option><option value="withdrawn">Withdrawn</option></select></div><div class="col-12 mt-3"><div class="form-group"><label for="InputRemarks">Remarks/ Notes:</label><textarea type="text" class="form-control" name="replier_remark[' + i +']" placeholder="Enter your Remarks/ Notes"></textarea></div></div><div class="col-md-12 mt-3"><label for="InputRemarks">Attachment</label><input  type="file"  name="replier_file_name[' + i +']" id="concreteFile" class="form-control" /></div></div><div class="col-md-12 mt-3"><button type="button" class="btn btn-outline-danger remove-input-field">Delete</button></div></td></tr>');
+    });
+    $(document).on('click', '.remove-input-field', function () {
+        $(this).parents('tr').remove();
+    });
+  });
+</script>
+@endpush
