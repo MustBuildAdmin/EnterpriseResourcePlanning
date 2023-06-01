c5908d0d243a0a9796e2a7730db8f1669f2040f7
unknown <balamurugan@mustbuildapp.com>
2023-06-01 20:31:46 +0530

    construction bug

--- resources/views/project_task_con/list.blade.php
+++ resources/views/project_task_con/list.blade.php
@@ -263,69 +263,67 @@
                                     </tr>
                                     </thead>
                                     <tbody class="list">
-                                        @if(count($tasks) > 0)
-                                            @foreach($tasks as $task)
-                                                @if($task->instance_id == $task->pro_instance_id)
-                                                    @if (DB::table('con_tasks')->where('project_id',$task->project_id)->where('parent',$task->id)->exists())
-                                                    @else
-                                                        <tr>
-                                                            <td>
-                                                                <span class="d-flex text-sm text-center justify-content-between">
-                                                                    <p class="m-0">{{ $task->project_name }}</p>
-                                                                    <span class="me-5 badge p-2 px-3 rounded bg-{{ (\Auth::user()->checkProject($task->project_id) == 'Owner') ? 'success' : 'warning'  }}">
-                                                                        {{ __(\Auth::user()->checkProject($task->project_id)) }}
-                                                                    </span>
+                                        @forelse ($tasks as $task)
+                                            @if($task->instance_id == $task->pro_instance_id)
+                                                @if (DB::table('con_tasks')->where('project_id',$task->project_id)->where('parent',$task->id)->exists())
+                                                @else
+                                                    <tr>
+                                                        <td>
+                                                            <span class="d-flex text-sm text-center justify-content-between">
+                                                                <p class="m-0">{{ $task->project_name }}</p>
+                                                                <span class="me-5 badge p-2 px-3 rounded bg-{{ (\Auth::user()->checkProject($task->project_id) == 'Owner') ? 'success' : 'warning'  }}">
+                                                                    {{ __(\Auth::user()->checkProject($task->project_id)) }}
                                                                 </span>
-                                                            </td>
-                                                            <td>
-                                                                <span class="h6 text-sm font-weight-bold mb-0">{{ $task->text }}</span>
-                                                            </td>
-                                                            <td class="{{ (strtotime($task->start_date) < time()) ? 'text-danger' : '' }}">
-                                                                {{ Utility::site_date_format($task->start_date,\Auth::user()->id) }}
-                                                            </td>
-                                                            <td class="{{ (strtotime($task->end_date) < time()) ? 'text-danger' : '' }}">
-                                                                {{ Utility::site_date_format($task->end_date,\Auth::user()->id) }}
-                                                            </td>
-                                                            <td>
-                                                                <div class="avatar-group">
-                                                                    @if($task->users()->count() > 0)
-                                                                        @if($users = $task->users()) 
-                                                                            @foreach($users as $key => $user)
-                                                                                @if($key<3)
-                                                                                    <a href="#" class="avatar rounded-circle avatar-sm">
-                                                                                        <img data-original-title="{{(!empty($user)?$user->name:'')}}" @if($user->avatar) src="{{asset('/storage/uploads/avatar/'.$user->avatar)}}" @else src="{{asset('/storage/uploads/avatar/avatar.png')}}" @endif title="{{ $user->name }}" class="hweb">
-                                                                                    </a>
-                                                                                @else
-                                                                                    @break
-                                                                                @endif
-                                                                            @endforeach
-                                                                        @endif
-                                                                        @if(count($users) > 3)
-                                                                            <a href="#" class="avatar rounded-circle avatar-sm">
-                                                                                <img  data-original-title="{{(!empty($user)?$user->name:'')}}" @if($user->avatar) src="{{asset('/storage/uploads/avatar/'.$user->avatar)}}" @else src="{{asset('/storage/uploads/avatar/avatar.png')}}" @endif class="hweb">
-                                                                            </a>
-                                                                        @endif
-                                                                    @else
-                                                                        {{ __('-') }}
+                                                            </span>
+                                                        </td>
+                                                        <td>
+                                                            <span class="h6 text-sm font-weight-bold mb-0">{{ $task->text }}</span>
+                                                        </td>
+                                                        <td class="{{ (strtotime($task->start_date) < time()) ? 'text-danger' : '' }}">
+                                                            {{ Utility::site_date_format($task->start_date,\Auth::user()->id) }}
+                                                        </td>
+                                                        <td class="{{ (strtotime($task->end_date) < time()) ? 'text-danger' : '' }}">
+                                                            {{ Utility::site_date_format($task->end_date,\Auth::user()->id) }}
+                                                        </td>
+                                                        <td>
+                                                            <div class="avatar-group">
+                                                                @if($task->users()->count() > 0)
+                                                                    @if($users = $task->users()) 
+                                                                        @foreach($users as $key => $user)
+                                                                            @if($key<3)
+                                                                                <a href="#" class="avatar rounded-circle avatar-sm">
+                                                                                    <img data-original-title="{{(!empty($user)?$user->name:'')}}" @if($user->avatar) src="{{asset('/storage/uploads/avatar/'.$user->avatar)}}" @else src="{{asset('/storage/uploads/avatar/avatar.png')}}" @endif title="{{ $user->name }}" class="hweb">
+                                                                                </a>
+                                                                            @else
+                                                                                @break
+                                                                            @endif
+                                                                        @endforeach
                                                                     @endif
-                                                                </div>
-                                                            </td>
-                                                            <td class="text-center w-15">
-                                                                <div class="actions">
-                                                                    <a data-size="lg" data-url="{{ route('project.taskboard.edit',['task_id'=>$task->main_id,'get_date'=>$get_date]) }}" data-ajax-popup="true" data-bs-toggle="tooltip" title="{{__('Edit')}}" data-title="{{__('Edit Task')}}" class="btn btn-sm btn-primary">
-                                                                        <i class="ti ti-pencil"></i>
-                                                                    </a>
-                                                                </div>
-                                                            </td>
-                                                        </tr>
-                                                    @endif
+                                                                    @if(count($users) > 3)
+                                                                        <a href="#" class="avatar rounded-circle avatar-sm">
+                                                                            <img  data-original-title="{{(!empty($user)?$user->name:'')}}" @if($user->avatar) src="{{asset('/storage/uploads/avatar/'.$user->avatar)}}" @else src="{{asset('/storage/uploads/avatar/avatar.png')}}" @endif class="hweb">
+                                                                        </a>
+                                                                    @endif
+                                                                @else
+                                                                    {{ __('-') }}
+                                                                @endif
+                                                            </div>
+                                                        </td>
+                                                        <td class="text-center w-15">
+                                                            <div class="actions">
+                                                                <a data-size="lg" data-url="{{ route('project.taskboard.edit',['task_id'=>$task->main_id,'get_date'=>$get_date]) }}" data-ajax-popup="true" data-bs-toggle="tooltip" title="{{__('Edit')}}" data-title="{{__('Edit Task')}}" class="btn btn-sm btn-primary">
+                                                                    <i class="ti ti-pencil"></i>
+                                                                </a>
+                                                            </div>
+                                                        </td>
+                                                    </tr>
                                                 @endif
-                                            @endforeach
-                                        @else
+                                            @endif
+                                        @empty
                                             <tr>
-                                                <th scope="col" colspan="6"><h6 class="text-center">{{__('No tasks found')}}</h6></th>
+                                                <td colspan="6"><h6 class="text-center">{{__('No tasks found')}}</h6></td>
                                             </tr>
-                                        @endif
+                                        @endforelse
                                     </tbody>
                                 </table>
                             </div>
@@ -404,7 +402,7 @@
                                                 @endforeach
                                             @else
                                                 <tr>
-                                                    <th scope="col" colspan="6"><h6 class="text-center">{{__('No tasks found')}}</h6></th>
+                                                    <td scope="col" colspan="6"><h6 class="text-center">{{__('No tasks found')}}</h6></td>
                                                 </tr>
                                             @endif
                                         </tbody>
