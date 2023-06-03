050e0710a657c2226a62c55478a3b473de20ba90
MustBuildAdmin <admin@mustbuildapp.com>
2023-06-04 00:28:51 +0530

    changes in css for create project button

--- resources/views/construction_project/construction_main.blade.php
+++ resources/views/construction_project/construction_main.blade.php
@@ -24,7 +24,11 @@
                 float: right;" data-url="{{ route('projects.create') }}" data-ajax-popup="true"
                     data-bs-toggle="tooltip" title="{{ __('Create New Project') }}">
                     <button type="button" class="btn btn-outline-primary w-20"
-                     ><i class="ti ti-plus mr-1"></i> {{ __('Create New Project') }}</button>
+                     ><svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <path d="M12 5l0 14"></path>
+                        <path d="M5 12l14 0"></path>
+                      </svg> {{ __('Create New Project') }}</button>
                 </a>
             @endcan
 
