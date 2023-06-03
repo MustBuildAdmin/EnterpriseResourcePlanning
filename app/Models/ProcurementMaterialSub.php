8e9dd2118751dc9fd2a922d687c8d3a9a1991f5e
unknown <vijay@mustbuildapp.com>
2023-06-03 21:41:09 +0530

    changes in code

---
+++ app/Models/ProcurementMaterialSub.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProcurementMaterialSub extends Model
+{
+    use HasFactory;
+    protected $table = 'procurement_material_submission';
+}
