85880f0eb02f28dc813340bb9d53493ff8a42115
mohan <mohank@yelagiriinfotech.com>
2023-06-12 21:00:50 +0530

    UI changes

--- resources/views/diary/consultant_direction/create.blade.php
+++ resources/views/diary/consultant_direction/create.blade.php
@@ -6,89 +6,92 @@
 <div class="modal-body">
   <div class="row">
     <div class="container">
-      <h3 style="text-align: center; font-weight: 800; color: blue"> Consultants Directions Summary</h3>
       <form class="" action="{{ route('save_consultant_direction') }}" enctype="multipart/form-data" method="POST"> 
         @csrf 
         <div class="container">
-          <div class="row mb-5">
+          <div class="row">
             <div class="col form-group ">
-              <label for="InputIssued">ARCHITECT AND ENGNEER'S DIRECTIONS (AD & ED) SUMMARY for the project of:</label>
+              <label for="InputIssued">{{__('ARCHITECT AND ENGNEERS DIRECTIONS (AD & ED) SUMMARY for the project of:')}}</label>
               <span>{{$project_name->project_name}}</span>
             </div>
           </div>
           <hr style="border: 1px solid black" />
-          <div class="row mb-5">
+          <div class="col-xs-12">
+            <h3 style="text-align: center;text-transform: uppercase;">{{__('Consultants Directions Summary')}}</h3>
+          </div>
+
+          <div class="row">
             <input type="hidden" name="project_id" value="{{$project}}">
             <div class="col">
               <div class="form-group">
-                <label for="InputIssued">Issued By:</label>
-                <input name="issued_by" type="text" class="form-control" placeholder="Enter your  Issued By" />
+                <label for="InputIssued">{{__('Issued By')}} <span style='color:red;'>*</span></label>
+                <input name="issued_by" type="text" class="form-control" placeholder="{{__('Issued By')}}" required/>
               </div>
             </div>
             <div class="col">
               <div class="form-group">
-                <label for="InputIssued">Issued Date:</label>
-                <input name="issued_date" type="date" class="form-control" placeholder="Enter your  Issued Date" />
+                <label for="InputIssued">{{__('Issued Date')}} <span style='color:red;'>*</span></label>
+                <input name="issued_date" type="date" class="form-control" placeholder="{{__('Issued Date')}}" required/>
               </div>
             </div>
             <div class="col">
               <div class="form-group">
-                <label for="InputReference">AD/ED Reference:</label>
-                <input name="ad_ae_ref" type="text" class="form-control" placeholder="Enter your  AD/ED Reference" />
+                <label for="InputReference">{{__('AD/ED Reference')}} <span style='color:red;'>*</span></label>
+                <input name="ad_ae_ref" type="text" class="form-control" placeholder="{{__('AD/ED Reference')}}" required/>
               </div>
             </div>
             <div class="col-12 mt-3">
               <div class="form-group">
-                <label for="InputDescription">AD/ED Description:</label>
-                <textarea name="ad_ae_decs" type="text" class="form-control" placeholder="Enter your  AD/ED Description"></textarea>
+                <label for="InputDescription">{{__('AD/ED Description')}} <span style='color:red;'>*</span></label>
+                <textarea name="ad_ae_decs" type="text" class="form-control" placeholder="{{__('AD/ED Description')}}" required></textarea>
               </div>
               <div class="col-md-12 mt-3">
-                <label for="InputRemarks">Attachment</label>
-                <input name="attach_file_name" required type="file" id="concreteFile" class="form-control" name="file" />
+                <label for="InputRemarks">{{__('Attachment')}} <span style='color:red;'>*</span></label>
+                <input name="attach_file_name" required type="file" id="concreteFile" class="form-control" required/>
               </div>
             </div>
           </div>
           <table class="table" id="dynamicAddRemove">
             <tr>
               <td>
-                <h4 style="text-align: center; font-weight: 700"> Initiator Action: </h4>
+                <h4 style="text-align: center; font-weight: 700">{{__('Initiator Action &  Reply')}}</h4>
                 <div class="row mb-5">
                   <div class="col">
                     <div class="form-group">
-                      <label for="InputReference">Reference:</label>
-                      <input type="text" name="initiator_reference[]" class="form-control" placeholder="Enter your  Reference" />
+                      <label for="InputReference">{{__('Reference')}}</label>
+                      <input type="text" name="initiator_reference[]" class="form-control" placeholder="{{__('Reference')}}" />
                     </div>
                   </div>
                   <div class="col">
                     <div class="form-group">
-                      <label for="Inputdate">Date:</label>
-                      <input type="date" name="initiator_date[]" class="form-control" placeholder="Enter your  Date" />
+                      <label for="Inputdate">{{__('Date')}}</label>
+                      <input type="date" name="initiator_date[]" class="form-control" placeholder="{{__('Date')}}" />
                     </div>
                   </div>
                   <div class="col-md-12 mt-3">
-                    <label for="InputRemarks">Attachment</label>
-                    <input name="initiator_file_name[]" required type="file" id="concreteFile" class="form-control" name="file" />
+                    <label for="InputRemarks">{{__('Attachment')}}</label>
+                    <input name="initiator_file_name[]"  type="file"  class="form-control"  />
                   </div>
                 </div>
-                <h4 style="text-align: center; font-weight: 700">Replier:</h4>
+                {{-- <h4 style="text-align: center; font-weight: 700;">Replier:</h4>
                 <div class="row mb-3">
                   <div class="col">
                     <div class="form-group">
-                      <label for="InputReference">Reference:</label>
-                      <input type="text" name="replier_reference[]" class="form-control" placeholder="Enter your  Reference" />
+                      <label for="InputReference">Reference <span style='color:red;'>*</span></label>
+                      <input type="text" name="replier_reference[]" class="form-control" placeholder="Enter your  Reference" required />
                     </div>
                   </div>
                   <div class="col">
                     <div class="form-group">
-                      <label for="Inputdate">Date:</label>
-                      <input type="date" name="replier_date[]" class="form-control" placeholder="Enter your  Date" />
+                      <label for="Inputdate">Date <span style='color:red;'>*</span></label>
+                      <input type="date" name="replier_date[]" class="form-control" placeholder="Enter your  Date" required/>
                     </div>
                   </div>
-                </div>
-                <div class="row mb-5">
+                </div> --}}
+                <div class="row">
                   <div class="col form-group">
-                    <label for="InputRemarks">Status:</label>
-                    <select name="replier_status[]" class="form-control" aria-label="Default select example">
+                    <label for="InputRemarks">{{__('Status')}}</label>
+                    <select name="replier_status[]" class="form-control" aria-label="Default select example" >
                       <option selected disabled>Status</option>
                       <option value="clear">Clear</option>
                       <option value="pending">Pending</option>
@@ -97,19 +100,19 @@
                   </div>
                   <div class="col-12 mt-3">
                     <div class="form-group">
-                      <label for="InputRemarks">Remarks/ Notes:</label>
-                      <textarea type="text" class="form-control" name="replier_remark[]" placeholder="Enter your Remarks/ Notes"></textarea>
+                      <label for="InputRemarks">{{__('Remarks/ Notes')}}</label>
+                      <textarea type="text" class="form-control" name="replier_remark[]" placeholder="{{__('Remarks/ Notes')}}" ></textarea>
                     </div>
                   </div>
                   <div class="col-md-12 mt-3">
-                    <label for="InputRemarks">Attachment</label>
-                    <input required type="file" name="replier_file_name[]" id="concreteFile" class="form-control" name="file" />
+                    <label for="InputRemarks">{{__('Attachment')}}</label>
+                    <input  type="file" name="replier_file_name[]" class="form-control"  />
                   </div>
                 </div>
               </td>
             </tr>
           </table>
-          <div class="row" style="margin:20px 0px 20px">
+          <div class="row">
             <div class="modal-footer">
               <input type="button" value="{{__('Cancel')}}" class="btn btn-light" data-bs-dismiss="modal">
               <input type="submit" value="{{__('Create')}}" class="btn  btn-primary">
