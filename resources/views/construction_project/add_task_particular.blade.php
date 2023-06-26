5df111601b0b0488402003336b8c7fadc1b53ae4
MustBuildAdmin <109482580+MustBuildAdmin@users.noreply.github.com>
2023-06-26 21:15:06 +0530

    Update add_task_particular.blade.php

--- resources/views/construction_project/add_task_particular.blade.php
+++ resources/views/construction_project/add_task_particular.blade.php
@@ -5,7 +5,7 @@
             <form action="{{route('con_taskupdate')}}" method="POST" enctype="multipart/form-data">
                 @csrf 
                 <div class="row min-750" id="taskboard_view">
-                    <div class="col-2">
+                    <div class="col-6">
                         <div class="form-group">
                             {{ Form::label('name', __('Date'),['class' => 'form-label']) }}<span class="text-danger">*</span>
                             {{ Form::date('get_date', $data['get_date'], array('class' => 'form-control month-btn','id' => 'get_date', 
@@ -16,7 +16,7 @@
                         </div>
                     </div>
 
-                    <div class="col-2">
+                    <div class="col-6">
                         <div class="form-group">
                             {{ Form::label('name', __('Percentage'),['class' => 'form-label']) }}<span class="text-danger">*</span>
                             {{ Form::number('percentage', null, ['class' => 'form-control','id' => 'percentage','required'=>'required','max'=>'100','min'=>'1']) }}
@@ -26,12 +26,12 @@
                     </div>
                     </div>
                    <div class="row min-750" id="taskboard_view">
-                        <div class="col-4">
+                        <div class="col-6">
                             <label for="input">{{__('Attachments')}}</label>
                             <input type="file" class="form-control" name="attachment_file_name[]" multiple>
                             <div class="file_name_show"></div>
                         </div>
-                        <div class="col-4">
+                        <div class="col-6">
                             <div class="form-group">
                                 {{ Form::label('description', __('Description'),['class' => 'form-label']) }} <span style='color:red;'>*</span>
                                 {{ Form::textarea('description', null, ['class' => 'form-control','id' => 'description','rows'=>'3','data-toggle' => 'autosize','required'=>'required']) }}
