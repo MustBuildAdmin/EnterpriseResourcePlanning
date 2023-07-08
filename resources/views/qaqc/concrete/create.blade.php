fe08f911b760c789f4889eda2c60d09e473e6b2a
unknown <vijay@mustbuildapp.com>
2023-07-08 18:13:43 +0530

    c

--- resources/views/qaqc/concrete/create.blade.php
+++ resources/views/qaqc/concrete/create.blade.php
@@ -11,7 +11,9 @@
 <div class="modal-body">
   <div class="row">
     <div class="container">
-      <h3 style="text-align: center; font-weight: 500">{{__('MONTHLY REPORT BY PROJECT ENGINEERS / SITE ENGINEERS')}} </h3>
+      <h3 style="text-align: center; font-weight: 500">
+        {{__('MONTHLY REPORT BY PROJECT ENGINEERS / SITE ENGINEERS')}} 
+      </h3>
      
       <form class="" action="{{ route('concrete.save_concrete_pouring') }}" enctype="multipart/form-data" method="POST">
         @csrf
