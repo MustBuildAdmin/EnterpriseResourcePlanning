631c4efd8dfece08cc04d83c8005a70b745f0cca
Amruth <amruthvinayak@gmail.com>
2023-07-12 22:35:34 +0530

    resource type changes 3

--- resources/views/hrm/system_setup/performance_type/performance_type_create.blade.php
+++ resources/views/hrm/system_setup/performance_type/performance_type_create.blade.php
@@ -18,7 +18,7 @@
 </div>
 <div class="modal-footer">
     <input type="button" value="{{__('Cancel')}}" class="btn  btn-light" data-bs-dismiss="modal">
-    <input type="submit" value="{{__('Create')}}" class="btn  btn-primary submit_button">
+    <input type="submit" id="add_performance_type" value="{{__('Create')}}" class="btn  btn-primary submit_button">
 </div>
 {{ Form::close() }}
 
@@ -45,6 +45,9 @@
                     alert("Request: "+JSON.stringify(request));
                 }
             });
+        });
+        $(document).on('submit', 'form', function() {
+            $('#add_performance_type').attr('disabled', 'disabled');
         });
     });
 </script>
