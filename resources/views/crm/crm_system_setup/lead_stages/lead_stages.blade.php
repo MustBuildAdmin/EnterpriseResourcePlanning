12c4405dedde47bbfd4f0e195d90717b2918140b
VijayMust <125757509+VijayMust@users.noreply.github.com>
2023-07-18 16:02:31 +0530

    Crm issues (#371)
    * crm changes
    
    * changes in sonarcloud
    
    * c
    
    * changes

--- resources/views/crm/crm_system_setup/lead_stages/lead_stages.blade.php
+++ resources/views/crm/crm_system_setup/lead_stages/lead_stages.blade.php
@@ -1,86 +1,99 @@
 @include('new_layouts.header')
-
 <style>
 .nav-fill .nav-item .nav-link, .nav-justified .nav-item .nav-link {
     float: right;
 }
+i.ti.ti-plus {
+    color: #FFF !important;
+}
 </style>
 
-<div class="page-wrapper"> 
+<div class="page-wrapper">
     @include('crm.side-menu')
-
-
-<div class="row">
-  <div class="col-md-6">
-     <h2>Manage Lead Stages</h2>
-  </div>
-  <div class="col-md-6 float-end ">
-       
-  <a class="floatrght btn btn-sm btn-primary" href="#" data-size="md" data-url="{{ route('lead_stages.create') }}" data-ajax-popup="true" data-bs-toggle="tooltip" title="{{__('Create Lead Stage')}}" >
-            <i class="ti ti-plus"></i>
-        </a>
-  </div>
-</div>
-
-
-
     <div class="row">
-        <div class="col-3">
-            @include('layouts.crm_setup')
-        </div>
-        <div class="col-9">
-            <div class="row justify-content-center">
-                <div class="p-3 card">
-                    <ul class="nav nav-pills nav-fill" id="pills-tab" role="tablist">
-                        @php($i=0)
-                        @foreach($pipelines as $key => $pipeline)
-                            <li class="nav-item" role="presentation">
-                                <button class="nav-link @if($i==0) active @endif" id="pills-user-tab-1" data-bs-toggle="pill"
-                                        data-bs-target="#tab{{$key}}" type="button">{{$pipeline['name']}}
-                                </button>
-                            </li>
-                            @php($i++)
-                        @endforeach
-                    </ul>
-                </div>
-                <div class="card">
-                    <div class="card-body">
-                        <div class="tab-content" id="pills-tabContent">
+       <div class="col-md-6">
+          <h2>{{__('Manage Lead Stages')}}</h2>
+       </div>
+       <div class="col-md-6 float-end ">
+          <a class="floatrght btn btn-sm btn-primary" href="#" data-size="md"
+             data-url="{{ route('lead_stages.create') }}" data-ajax-popup="true"
+             data-bs-toggle="tooltip" title="{{__('Create Lead Stage')}}" >
+          <i class="ti ti-plus"></i>
+          </a>
+       </div>
+    </div>
+        <div class="row">
+            <div class="col-3">
+                @include('layouts.crm_setup')
+            </div>
+            <div class="col-9">
+                <div class="row justify-content-center">
+                    <div class="p-3 card">
+                        <ul class="nav nav-pills nav-fill" id="pills-tab" role="tablist">
                             @php($i=0)
                             @foreach($pipelines as $key => $pipeline)
-                                <div class="tab-pane fade show @if($i==0) active @endif" id="tab{{$key}}" role="tabpanel" aria-labelledby="pills-user-tab-1">
-                                    <ul class="list-group sortable">
-                                        @foreach ($pipeline['lead_stages'] as $lead_stages)
-                                            <li class="list-group-item" data-id="{{$lead_stages->id}}">
-                                                <span class="text-xs text-dark">{{$lead_stages->name}}</span>
-                                                <span class="float-end">
-                                                    <div class="ms-2" style="display:flex;gap:10px;">
-                                                        @can('edit lead stage')
-                                                            <a href="#" class="btn btn-md" data-url="{{ URL::to('lead_stages/'.$lead_stages->id.'/edit') }}" data-ajax-popup="true" data-size="md" data-bs-toggle="tooltip" title="{{__('Edit')}}" data-title="{{__('Edit Lead Stages')}}">
-                                                                <i class="ti ti-pencil text-white"></i>
-                                                            </a>
-                                                        @endcan
-                                                        @if(count($pipeline['lead_stages']))
-                                                            @can('delete lead stage')
-                                                                {!! Form::open(['method' => 'DELETE', 'route' => ['lead_stages.destroy', $lead_stages->id]]) !!}
-                                                                    <a href="#" class="btn btn-md btn-danger bs-pass-para" data-bs-toggle="tooltip" title="{{__('Delete')}}"><i class="ti ti-trash text-white"></i></a>
-                                                                {!! Form::close() !!}
-                                                            @endcan
-                                                        @endif
-                                            </span>
-                                            </li>
-                                        @endforeach
-                                    </ul>
-                                </div>
-                                @php($i++)
+                            <li class="nav-item" role="presentation">
+                            <button class="nav-link @if($i==0) active @endif"
+                                id="pills-user-tab-1" data-bs-toggle="pill"
+                                data-bs-target="#tab{{$key}}" type="button">{{$pipeline['name']}}
+                            </button>
+                            </li>
+                            @php($i++)
                             @endforeach
+                        </ul>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="tab-content" id="pills-tabContent">
+                                @php($i=0)
+                                @foreach($pipelines as $key => $pipeline)
+                                    <div class="tab-pane fade show @if($i==0) active @endif"
+                                        id="tab{{$key}}" role="tabpanel" aria-labelledby="pills-user-tab-1">
+                                        <ul class="list-group sortable">
+                                            @foreach ($pipeline['lead_stages'] as $lead_stages)
+                                                <li class="list-group-item" data-id="{{$lead_stages->id}}">
+                                                    <span class="text-xs text-dark">{{$lead_stages->name}}</span>
+                                                    <span class="float-end">
+                                                        <div class="ms-2" style="display:flex;gap:10px;">
+                                                            @can('edit lead stage')
+                                                            <a href="#" class="btn btn-md"
+                                                                data-url="{{ URL::to('lead_stages/'.$lead_stages->id.'
+                                                                /edit') }}" data-ajax-popup="true" data-size="md"
+                                                                data-bs-toggle="tooltip" title="{{__('Edit')}}"
+                                                                data-title="{{__('Edit Lead Stages')}}">
+                                                            <i class="ti ti-pencil text-white"></i>
+                                                            </a>
+                                                            @endcan
+                                                            @if(count($pipeline['lead_stages']))
+                                                                @can('delete lead stage')
+                                                                {!! Form::open(['method' => 'DELETE',
+                                                                'route' => ['lead_stages.destroy',
+                                                                $lead_stages->id]])
+                                                                !!}
+                                                                <a href="#" class="btn btn-md btn-danger
+                                                                    bs-pass-para" data-bs-toggle="tooltip"
+                                                                    title="{{__('Delete')}}">
+                                                                    <i class="ti ti-trash text-white"></i>
+                                                                </a>
+                                                                {!! Form::close() !!}
+                                                                @endcan
+                                                            @endif
+                                                    </span>
+                                                </li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                    @php($i++)
+                                @endforeach
+                            </div>
+                                <p class="text-muted mt-4"><strong>{{__('Note')}} : </strong>
+                                {{__('You can easily change order of lead stage using drag & drop.')}}
+                                </p>
                         </div>
-                        <p class="text-muted mt-4"><strong>{{__('Note')}} : </strong>{{__('You can easily change order of lead stage using drag & drop.')}}</p>
                     </div>
                 </div>
             </div>
         </div>
-    </div>  
 @include('new_layouts.footer')
 
 <script src="{{ asset('js/jquery-ui.min.js') }}"></script>
