feaab1181049cb89ebe906ea4ad168e1c10098cb
balamurugan1998 <murugan20011998@gmail.com>
2023-08-14 16:30:50 +0530

    erp

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -13,9 +13,87 @@
     .estimated_days:hover {
         border: 1px solid #ccc !important;
     }
+
+    /* Loader */
+    .wrappers{
+        display: flex;
+        justify-content: center;
+    }
+    .cards {
+        display: flex;
+        padding: 24px;
+        border-radius: 5px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .loader{
+        border-radius: 50%;
+        position: relative;
+        display: inline-block;
+        height: 0px;
+        width: 0px;
+    }
+
+    .loader span{
+        position: absolute;
+        display: block;
+        background: #ddd;
+        height: 15px;
+        width: 15px;
+        border-radius: 50%;
+        top: -20px;
+        perspective: 100000px;
+    }
+    .loader span:nth-child(1) {
+        left:30px;
+        animation: bounce2 1s cubic-bezier(0.04, 0.35, 0, 1) infinite;
+        animation-delay: 0s;
+        background: #ff756f;
+    }
+    .loader span:nth-child(2) {
+        left:6px;
+        animation: bounce2 1s cubic-bezier(0.04, 0.35, 0, 1) infinite;
+        animation-delay: .2s;
+        background: #ffde6f;
+    }
+    .loader span:nth-child(3) {
+        left:-20px;
+        animation: bounce2 1s cubic-bezier(0.04, 0.35, 0, 1) infinite;
+        animation-delay: .4s;
+        background: #01de6f;
+    }
+    .loader span:nth-child(4) {
+        left: -44px;
+        animation: bounce2 1s cubic-bezier(0.04, 0.35, 0, 1) infinite;
+        animation-delay: .6s;
+        background: #6f75ff;
+    }
+
+    @keyframes bounce2 {
+        0%, 56%, 100% {
+            transform: translateY(0px);
+        }
+        25% {
+            transform: translateY(-30px);
+        }
+    }
 </style>
 <div class="modal-body">
     <div class="container">
+        <center>
+            <section class="wrappers loader_show_hide" style="display: none;">
+                <div class="cards">
+                  <div class="loader">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                  </div>
+                </div>
+            </section>
+        </center>
+
         {{ Form::open(['url' => 'projects', 'method' => 'post','enctype' => 'multipart/form-data', 'id' => 'create_project_form', 'class' => 'create_project_form']) }}
             <div>
                 <h3>{{ __('Project Details') }}</h3>
@@ -461,6 +539,7 @@
                     reverseButtons: true
                 }).then((result) => {
                     if (result.isConfirmed) {
+                        var finishButton = form.find('a[href="#finish"]').removeAttr('href');
                         form.submit();
                     }
                     else if (result.dismiss === Swal.DismissReason.cancel) {
