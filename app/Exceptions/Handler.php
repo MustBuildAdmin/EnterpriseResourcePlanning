634dcc9d739a043d1e26f6aed65bb957f4c1564a
Must BuildAdmin <admin@mustbuildapp.com>
2023-08-25 06:08:52 +0000

    added sentry

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -37,7 +37,8 @@ class Handler extends ExceptionHandler
         
         $this->reportable(function (Throwable $e) {
             if (app()->bound('sentry')) {
-                app('sentry')->captureException($e);
-              }
+            app('sentry')->captureException($e);
+          }
         });
+    }
 }
