10c6db10dd2dd46f4451b42dcb6c5e92a7902b47
MustBuildAdmin <admin@mustbuildapp.com>
2023-08-29 22:31:49 +0530

    changes in the navbar

--- resources/views/new_layouts/header.blade.php
+++ resources/views/new_layouts/header.blade.php
@@ -111,6 +111,62 @@
 							@endif
                     </a>
                 </h1>
+				<div class="collapse navbar-collapse" id="navbar-menu">
+				<div class="navbar navbar-light">
+					<div class="container-fluid">
+						<ul class="navbar-nav">
+							<li class="nav-item">
+								<a class="nav-link" href="{{route('new_home')}}"> <span class="nav-link-icon d-md-none d-lg-inline-block">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24"
+                                            height="24" viewBox="0 0 24 24" stroke-width="2"
+                                            stroke="currentColor" fill="none" stroke-linecap="round"
+                                            stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                            <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
+                                            <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
+                                            <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
+                                        </svg>
+                                    </span> <span class="nav-link-title">
+                                        {{__('Home')}}
+                                    </span> </a>
+							</li>
+						</ul>
+						{{-- <div class="my-2 my-md-0 flex-grow-1 flex-md-grow-0 order-first order-md-last">
+							<form action="./" method="get" autocomplete="off" novalidate>
+								<div class="input-icon"> <span class="input-icon-addon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24"
+                                            height="24" viewBox="0 0 24 24" stroke-width="2"
+                                            stroke="currentColor" fill="none" stroke-linecap="round"
+                                            stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                            <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+                                            <path d="M21 21l-6 -6" />
+                                        </svg>
+                                    </span>
+									<input type="text" value="" class="form-control" placeholder="Search…" aria-label="Search in website"> </div>
+							</form>
+						</div> --}}
+						<div class="dropdown dash-h-item drp-language order-md-last">
+							<a class="dash-head-link dropdown-toggle arrow-none me-0" id="lang" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+								<i class="fas fa-globe"></i>
+                                <span class="drp-text hide-mob">{{Str::upper(isset($lang)?$lang:'en')}}</span>
+
+                            </a>
+							<div class="dropdown-menu dash-h-dropdown dropdown-menu-end">
+                                @foreach($languages as $language)
+                                    <a href="{{route('change.language',$language)}}"  class="dropdown-item @if($language == $lang) text-danger @endif">
+                                        <span>{{Str::upper($language)}}</span>
+                                    </a>
+                                @endforeach
+								<h></h>
+                                @if(\Auth::user()->type=='super admin')
+                                <a class="dropdown-item text-primary" href="{{route('manage.language',[isset($lang)?$lang:'en'])}}">{{ __('Manage Language ') }}</a>
+                                @endif
+                            </div>
+						</div>
+					</div>
+				</div>
+			</div>
 				<div class="navbar-nav flex-row order-md-last">
 					<div class="nav-item d-none d-md-flex me-3"> </div>
 					<div class="d-none d-md-flex">
@@ -222,66 +278,7 @@
 				</div>
 			</div>
 		</header>
-		<header class="navbar-expand-md">
-			<div class="collapse navbar-collapse" id="navbar-menu">
-				<div class="navbar navbar-light">
-					<div class="container-fluid">
-						<ul class="navbar-nav">
-							<li class="nav-item">
-								<a class="nav-link" href="{{route('new_home')}}"> <span class="nav-link-icon d-md-none d-lg-inline-block">
-                                        <!-- Download SVG icon from http://tabler-icons.io/i/home -->
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24"
-                                            height="24" viewBox="0 0 24 24" stroke-width="2"
-                                            stroke="currentColor" fill="none" stroke-linecap="round"
-                                            stroke-linejoin="round">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                            <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
-                                            <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
-                                            <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
-                                        </svg>
-                                    </span> <span class="nav-link-title">
-                                        {{__('Home')}}
-                                    </span> </a>
-							</li>
-						</ul>
-						{{-- <div class="my-2 my-md-0 flex-grow-1 flex-md-grow-0 order-first order-md-last">
-							<form action="./" method="get" autocomplete="off" novalidate>
-								<div class="input-icon"> <span class="input-icon-addon">
-                                        <!-- Download SVG icon from http://tabler-icons.io/i/search -->
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24"
-                                            height="24" viewBox="0 0 24 24" stroke-width="2"
-                                            stroke="currentColor" fill="none" stroke-linecap="round"
-                                            stroke-linejoin="round">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                            <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
-                                            <path d="M21 21l-6 -6" />
-                                        </svg>
-                                    </span>
-									<input type="text" value="" class="form-control" placeholder="Search…" aria-label="Search in website"> </div>
-							</form>
-						</div> --}}
-						<div class="dropdown dash-h-item drp-language order-md-last">
-							<a class="dash-head-link dropdown-toggle arrow-none me-0" id="lang" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-								<i class="fas fa-globe"></i>
-                                <span class="drp-text hide-mob">{{Str::upper(isset($lang)?$lang:'en')}}</span>
-
-                            </a>
-							<div class="dropdown-menu dash-h-dropdown dropdown-menu-end">
-                                @foreach($languages as $language)
-                                    <a href="{{route('change.language',$language)}}"  class="dropdown-item @if($language == $lang) text-danger @endif">
-                                        <span>{{Str::upper($language)}}</span>
-                                    </a>
-                                @endforeach
-								<h></h>
-                                @if(\Auth::user()->type=='super admin')
-                                <a class="dropdown-item text-primary" href="{{route('manage.language',[isset($lang)?$lang:'en'])}}">{{ __('Manage Language ') }}</a>
-                                @endif
-                            </div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</header>
+	
 
 		<div class="modal fade" id="commonModal" tabindex="-1" role="dialog"
 			aria-labelledby="exampleModalLabel" aria-hidden="true">
