9d6bbbd6143e333a92bb62e70b338b2880de7fd2
vijay620 <smartvijay018@gmail.com>
2023-08-30 19:07:31 +0530

    alignment

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -1,35 +1,35 @@
 @include('new_layouts.header')
 <style>
-    #create {
-        height: 35px !important;
-        width: 12% !important;
-    }
+#create {
+    height: 35px !important;
+    width: 12% !important;
+}
 
-    #search_button {
-        height: 35px !important;
-        width: 12% !important;
-    }
+#search_button {
+    height: 35px !important;
+    width: 12% !important;
+}
 
-    #reset {
-        width: 12% !important;
-    }
+#reset {
+    width: 12% !important;
+}
 
-    .user-initial {
-        width: 101px;
-        height: 100px;
-        border-radius: 50%;
-        background-color: #e0e0e0;
-        color: #fff;
-        font-size: 42px;
-        text-align: center;
-    }
+.user-initial {
+    width: 101px;
+    height: 100px;
+    border-radius: 50%;
+    background-color: #e0e0e0;
+    color: #fff;
+    font-size: 42px;
+    text-align: center;
+}
 
-    .avatar-xl {
-        --tblr-avatar-size: 6.2rem;
-    }
+.avatar-xl {
+    --tblr-avatar-size: 6.2rem;
+}
 </style>
 @php
-    $profile = \App\Models\Utility::get_file('uploads/avatar');
+    $profile = \App\Models\Utility::get_file('uploads/avatar/');
 @endphp
 @include('crm.side-menu')
 <div class="row">
@@ -210,18 +210,18 @@
 </div>
 <script>
 $(document).on('change', '.document_setup', function(){
-	var fileExtension = ['jpeg', 'jpg', 'png', 'pdf', 'gif'];
-	if ($.inArray($(this).val().split('.').pop().toLowerCase(), fileExtension) == -1) {
-		$(".show_document_file").hide();
-		$(".show_document_error").html("Upload only pdf, jpeg, jpg, png");
-		$('input[type="submit"]').prop('disabled',true);
-		return false;
-	} else{
-		$(".show_document_file").show();
-		$(".show_document_error").hide();
-		$('input[type="submit"]').prop('disabled',false);
-		return true;
-	}
+    var fileExtension = ['jpeg', 'jpg', 'png', 'pdf', 'gif'];
+        if ($.inArray($(this).val().split('.').pop().toLowerCase(), fileExtension) == -1) {
+            $(".show_document_file").hide();
+            $(".show_document_error").html("Upload only pdf, jpeg, jpg, png");
+            $('input[type="submit"]').prop('disabled',true);
+            return false;
+        } else{
+            $(".show_document_file").show();
+            $(".show_document_error").hide();
+            $('input[type="submit"]').prop('disabled',false);
+            return true;
+        }
 });
 </script>
 @include('new_layouts.footer')
