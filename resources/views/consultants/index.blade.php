17b9e3320bb99b373d148c2a3a57c7c0cb43e595
vijay620 <smartvijay018@gmail.com>
2023-09-05 21:43:25 +0530

    changes in sonar cloud

--- resources/views/consultants/index.blade.php
+++ resources/views/consultants/index.blade.php
@@ -1,38 +1,34 @@
 @include('new_layouts.header')
 <style>
-	#create {
-		height: 35px !important;
-		width: 12% !important;
-	}
-	#invite{
-		height: 35px !important;
-		width: 12% !important;
-	}
+#create {
+  height:35px !important;
+  width:12% !important;
+}
+#invite {
+  height:35px !important;
+  width:12% !important;
+}
+#reset {
+  width:12% !important;
+}
+#search_button {
+  height:35px !important;
+  width:12% !important;
+}
+.dropdown-toggle::after {
+  display:none;
+  position:absolute;
+  top:50%;
+  right:20px;
+}
+.avatar.avatar-xl.mb-3.user-initial {
+  border-radius:50%;
+  color:#FFF;
+}
+.avatar-xl {
+  --tblr-avatar-size:6.2rem;
+}
 
-	#reset{
-		
-		width: 12% !important;
-	}
-
-	#search_button {
-		height: 35px !important;
-		width: 12% !important;
-	}
-
-	.dropdown-toggle::after {
-		display: none;
-		position: absolute;
-		top: 50%;
-		right: 20px;
-	}
-	.avatar.avatar-xl.mb-3.user-initial {
-		border-radius: 50%;
-		color: #FFF;
-	}
-
-	.avatar-xl {
-		--tblr-avatar-size: 6.2rem;
-	}
 </style>
 @php
     $profile=\App\Models\Utility::get_file('uploads/avatar/');
@@ -40,151 +36,177 @@
 <div class="page-wrapper">
 	<!-- Page header -->
 	<div class="page-header d-print-none">
-		<div class="container-xl">
-			<div class="row g-2 align-items-center">
-				<div class="col">
-					<h2 class="page-title">
-                {{__('Manage Consultants')}}
-
-            </h2> </div>
-				<!-- Page title actions -->
-				<div class="col-auto ms-auto d-print-none">
-					<form action="{{ route('consultants.index') }}" method="GET">
-						<div class="input-group">
-							{{ Form::text('search',isset($_GET['search'])?$_GET['search']:'',
-							array('class' => 'form-control d-inline-block w-9 me-3 mt-auto',
-							'id'=>'search','placeholder'=>__('Search by Name'))) }}
-								<div class="input-group-btn">
-									<button type="submit" id="search_button" class="btn btn-info">
-										<i class="fa fa-search" aria-hidden="true"></i>
-									</button>
-							{!! Form::close() !!}
-									<a href="{{ route('users.index') }}" id="reset" class="btn btn-danger"
-									 data-bs-toggle="tooltip" title="{{ __('Reset') }}">
-										<span class="btn-inner--icon">
-											<i class="ti ti-arrow-back"></i>
-										</span>
-									</a>
-										@can('create consultant')
-									<a href="#" class="btn btn-primary" data-size="lg" data-url="{{ route('consultants.create') }}"
-									 data-ajax-popup="true" data-bs-toggle="tooltip" title="{{__('Create New Consultant')}}"
-									  id="create" class="btn btn-primary">
-										<span class="btn-inner--icon">
-											<i class="fa fa-plus"></i>
-										</span>
-									</a>
-									<a href="{{ route('consultant.scott-search') }}" class="btn btn-primary" data-bs-toggle="tooltip"
-									title="{{__('Invite Consultant')}}"
-									  id="invite" class="btn btn-primary">
-										<span class="btn-inner--icon">
-											<i class="ti ti-send"></i>
-										</span>
-									</a>
-									@endcan
-								</div>
-						</div>
+	  <div class="container-xl">
+		<div class="row g-2 align-items-center">
+		  <div class="col">
+			<h2 class="page-title">
+			  {{__('Manage Consultants')}}
+			</h2>
+		  </div>
+		  <!-- Page title actions -->
+		  <div class="col-auto ms-auto d-print-none">
+			<form action="{{ route('consultants.index') }}" method="GET">
+			  <div class="input-group">
+				{{ Form::text('search',isset($_GET['search'])?$_GET['search']:'', array('class'
+				=> 'form-control d-inline-block w-9 me-3 mt-auto', 'id'=>'search','placeholder'=>__('Search
+				by Name'))) }}
+				<div class="input-group-btn">
+				  <button type="submit" id="search_button" class="btn btn-info">
+					<i class="fa fa-search" aria-hidden="true">
+					</i>
+				  </button>
+				  {!! Form::close() !!}
+				  <a href="{{ route('users.index') }}" id="reset" class="btn btn-danger"
+				  data-bs-toggle="tooltip" title="{{ __('Reset') }}">
+					<span class="btn-inner--icon">
+					  <i class="ti ti-arrow-back">
+					  </i>
+					</span>
+				  </a>
+				  @can('create consultant')
+				  <a href="#" class="btn btn-primary" data-size="lg" data-url="{{ route('consultants.create') }}"
+				  data-ajax-popup="true" data-bs-toggle="tooltip" title="{{__('Create New Consultant')}}"
+				  id="create" class="btn btn-primary">
+					<span class="btn-inner--icon">
+					  <i class="fa fa-plus">
+					  </i>
+					</span>
+				  </a>
+				  <a href="{{ route('consultant.scott-search') }}" class="btn btn-primary"
+				  data-bs-toggle="tooltip" title="{{__('Invite Consultant')}}" id="invite"
+				  class="btn btn-primary">
+					<span class="btn-inner--icon">
+					  <i class="ti ti-send">
+					  </i>
+					</span>
+				  </a>
+				  @endcan
 				</div>
-			</div>
+			  </div>
+		  </div>
 		</div>
+	  </div>
 	</div>
 	<!-- Page body -->
 	<div class="page-body">
-		<div class="container-xl">
-			<div class="row row-cards">
-				@forelse($users as $user)
-				<div class="col-md-6 col-lg-3">
-					<div class="card">
-						@if(Gate::check('edit consultant') || Gate::check('delete consultant'))
-						<div class="card-header-right">
-							<div class="btn-group card-option float-end">
-								<button type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown"
-								 aria-haspopup="true" aria-expanded="false">
-									<i class="ti ti-dots-vertical"></i>
-								</button>
-									@if ($user->color_code!=null || $user->color_code!='')
-											@php $color_co =$user->color_code; @endphp
-									@else
-											@php $color_co =Utility::rndRGBColorCode(); @endphp
-									@endif
-								<div class="dropdown-menu dropdown-menu-end">
-									@can('edit consultant')
-									<a href="#!" data-size="lg" data-url="{{ route('consultants.edit.new',[$user->id,$color_co]) }}"
-										 data-ajax-popup="true" class="dropdown-item" data-bs-original-title="{{__('Edit Consultant')}}">
-										 <i class="ti ti-pencil"></i>
-										 <span>{{__('Edit')}}</span>
-										</a>
-									@endcan
-									<a href="#!" data-url="{{route('consultants.reset',\Crypt::encrypt($user->id))}}"
-										 data-ajax-popup="true" data-size="md" class="dropdown-item"
-										 data-bs-original-title="{{__('Reset Password')}}">
-										 <i class="ti ti-adjustments"></i>
-										 <span>  {{__('Reset Password')}}</span>
-									</a>
-								</div>
-							</div>
-						</div> @endif
-						<div class="card-body p-4 text-center">
-							<?php  $short=substr($user->name, 0, 1);?>
-								<?php  $short_lname=substr($user->lname, 0, 1);?>
-								 @if(!empty($user->avatar))
-								 	<img src="{{(!empty($user->avatar))? $profile.$user->avatar :
-								 	asset(Storage::url(" uploads/avatar/avatar.png "))}}" class="avatar avatar-xl mb-3 rounded" alt="">
-								  @else
-										
-										<div class="avatar avatar-xl mb-3 user-initial" style='background-color:{{$color_co}}'>
-											{{strtoupper($short)}}{{strtoupper($short_lname)}}
-										</div>
-								  @endif
-									<?php $name = strlen($user->name) > 20 ? substr($user->name,0,19)."..." : $user->name;?>
-										<h3 class="m-0 mb-1">
-									<a href="#">{{ $name }}</a>
-								</h3>
-							</div>
-							@if(\Auth::user()->type != 'super admin')
-						<div class="d-flex">
-							<a data-bs-toggle="tooltip" data-copy_email="{{ $user->email }}" title="{{ $user->email }}"
-								 href="#" class="card-btn" onclick="copyToClipboard(this)">
-								<!-- Download SVG icon from http://tabler-icons.io/i/mail -->
-								<svg xmlns="http://www.w3.org/2000/svg" class="icon me-2 text-muted" width="24" height="24"
-								 viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-								  stroke-linecap="round" stroke-linejoin="round">
-									<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-									<path d="M3 5m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z" />
-									<path d="M3 7l9 6l9 -6" /> </svg> {{__('Email')}} </a>
-							<a data-bs-toggle="tooltip" data-copy_phone="{{ $user->phone }}" title="{{ $user->phone }}"
-								 class="card-btn" onclick="copyToClipboardphone(this)">
-								<!-- Download SVG icon from http://tabler-icons.io/i/phone -->
-								<svg xmlns="http://www.w3.org/2000/svg" class="icon me-2 text-muted" width="24" height="24"
-								 viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-								  stroke-linecap="round" stroke-linejoin="round">
-									<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-									<path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15
-									 -15a2 2 0 0 1 2 -2" /> </svg> {{__('Mobile')}}
-							</a>
-						</div>
-						@else
-						<div class="row justify-content-between align-items-center"> </div>
-						@endif
-					</div>
+	  <div class="container-xl">
+		<div class="row row-cards">
+		  @forelse($users as $user)
+		  <div class="col-md-6 col-lg-3">
+			<div class="card">
+			  @if(Gate::check('edit consultant') || Gate::check('delete consultant'))
+			  <div class="card-header-right">
+				<div class="btn-group card-option float-end">
+				  <button type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown"
+				  aria-haspopup="true" aria-expanded="false">
+					<i class="ti ti-dots-vertical">
+					</i>
+				  </button>
+				  @if ($user->color_code!=null || $user->color_code!='') @php $color_co
+				  =$user->color_code; @endphp @else @php $color_co =Utility::rndRGBColorCode();
+				  @endphp @endif
+				  <div class="dropdown-menu dropdown-menu-end">
+					@can('edit consultant')
+					<a href="#!" data-size="lg" data-url="{{ route('consultants.edit.new',[$user->id,$color_co]) }}"
+					data-ajax-popup="true" class="dropdown-item" data-bs-original-title="{{__('Edit Consultant')}}">
+					  <i class="ti ti-pencil">
+					  </i>
+					  <span>
+						{{__('Edit')}}
+					  </span>
+					</a>
+					@endcan
+					<a href="#!" data-url="{{route('consultants.reset',\Crypt::encrypt($user->id))}}"
+					data-ajax-popup="true" data-size="md" class="dropdown-item" data-bs-original-title="{{__('Reset Password')}}">
+					  <i class="ti ti-adjustments">
+					  </i>
+					  <span>
+						{{__('Reset Password')}}
+					  </span>
+					</a>
+				  </div>
 				</div>
-				@empty
-				<div class="page-body">
-					<div class="container-xl d-flex flex-column justify-content-center">
-						<div class="empty">
-							<div class="empty-img">
-								<img src="{{ asset('assets/images/undraw_printing_invoices_5r4r.svg') }}" height="128" alt="">
-							</div>
-							<p class="empty-title"> {{__('No Consultants Found')}}</p>
-						</div>
+			  </div>
+			  @endif
+			  <div class="card-body p-4 text-center">
+				<?php $short=substr($user->
+				  name, 0, 1);?>
+				  <?php $short_lname=substr($user->
+					lname, 0, 1);?> @if(!empty($user->avatar))
+					<img src="{{(!empty($user->avatar))? $profile.$user->avatar :
+					asset(Storage::url(" uploads/avatar/avatar.png "))}}" class="avatar avatar-xl mb-3 rounded"
+					alt="">
+					@else
+					<div class="avatar avatar-xl mb-3 user-initial" style='background-color:{{$color_co}}'>
+					  {{strtoupper($short)}}{{strtoupper($short_lname)}}
 					</div>
-				</div> @endforelse </div>
-			<div class="d-flex mt-4">
-				<ul class="pagination ms-auto"> {!! $users->links() !!} </ul>
+					@endif
+					<?php $name=s trlen($user->
+					  name) > 20 ? substr($user->name,0,19)."..." : $user->name;?>
+					  <h3 class="m-0 mb-1">
+						<a href="#">
+						  {{ $name }}
+						</a>
+					  </h3>
+			  </div>
+			  @if(\Auth::user()->type != 'super admin')
+			  <div class="d-flex">
+				<a data-bs-toggle="tooltip" data-copy_email="{{ $user->email }}" title="{{ $user->email }}"
+				href="#" class="card-btn" onclick="copyToClipboard(this)">
+				  <!-- Download SVG icon from http://tabler-icons.io/i/mail -->
+				  <svg xmlns="http://www.w3.org/2000/svg" class="icon me-2 text-muted" width="24"
+				  height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+				  fill="none" stroke-linecap="round" stroke-linejoin="round">
+					<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+					<path d="M3 5m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z"
+					/>
+					<path d="M3 7l9 6l9 -6" />
+				  </svg>
+				  {{__('Email')}}
+				</a>
+				<a data-bs-toggle="tooltip" data-copy_phone="{{ $user->phone }}" title="{{ $user->phone }}"
+				class="card-btn" onclick="copyToClipboardphone(this)">
+				  <!-- Download SVG icon from http://tabler-icons.io/i/phone -->
+				  <svg xmlns="http://www.w3.org/2000/svg" class="icon me-2 text-muted" width="24"
+				  height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+				  fill="none" stroke-linecap="round" stroke-linejoin="round">
+					<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+					<path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15
+					-15a2 2 0 0 1 2 -2" />
+				  </svg>
+				  {{__('Mobile')}}
+				</a>
+			  </div>
+			  @else
+			  <div class="row justify-content-between align-items-center">
+			  </div>
+			  @endif
 			</div>
+		  </div>
+		  @empty
+		  <div class="page-body">
+			<div class="container-xl d-flex flex-column justify-content-center">
+			  <div class="empty">
+				<div class="empty-img">
+				  <img src="{{ asset('assets/images/undraw_printing_invoices_5r4r.svg') }}"
+				  height="128" alt="">
+				</div>
+				<p class="empty-title">
+				  {{__('No Consultants Found')}}
+				</p>
+			  </div>
+			</div>
+		  </div>
+		  @endforelse
 		</div>
+		<div class="d-flex mt-4">
+		  <ul class="pagination ms-auto">
+			{!! $users->links() !!}
+		  </ul>
+		</div>
+	  </div>
 	</div>
-</div>
-
+  </div>
 @include('new_layouts.footer')
 <script>
 function copyToClipboard(element) {
@@ -206,25 +228,26 @@ function copyToClipboardphone(element) {
   toastr.success("Copying to clipboard was successful!");
 }
 
-$(document).on('keypress', function (e) {
-        if (e.which == 13) {
-            swal.closeModal();
-        }
+$(document).on('keypress',
+function(e) {
+  if (e.which == 13) {
+    swal.closeModal();
+  }
 });
 
-
-$(document).on('change', '.document_setup', function(){
-	var fileExtension = ['jpeg', 'jpg', 'png', 'pdf', 'gif'];
-	if ($.inArray($(this).val().split('.').pop().toLowerCase(), fileExtension) == -1) {
-		$(".show_document_file").hide();
-		$(".show_document_error").html("Upload only pdf, jpeg, jpg, png");
-		$('input[type="submit"]').prop('disabled',true);
-		return false;
-	} else{
-		$(".show_document_file").show();
-		$(".show_document_error").hide();
-		$('input[type="submit"]').prop('disabled',false);
-		return true;
-	}
+$(document).on('change', '.document_setup',
+function() {
+  var fileExtension = ['jpeg', 'jpg', 'png', 'pdf', 'gif'];
+  if ($.inArray($(this).val().split('.').pop().toLowerCase(), fileExtension) == -1) {
+    $(".show_document_file").hide();
+    $(".show_document_error").html("Upload only pdf, jpeg, jpg, png");
+    $('input[type="submit"]').prop('disabled', true);
+    return false;
+  } else {
+    $(".show_document_file").show();
+    $(".show_document_error").hide();
+    $('input[type="submit"]').prop('disabled', false);
+    return true;
+  }
 });
 </script>
--- resources/views/consultants/view.blade.php
+++ resources/views/consultants/view.blade.php
@@ -4,7 +4,8 @@
         <div class="col-md-6">
             <div class="form-group ">
                 {{Form::label('name',__('Name'),['class'=>'form-label']) }}
-                {{Form::text('name',null,array('class'=>'form-control font-style','disabled'=>'disabled','maxlength' => 35,'placeholder'=>__('Enter User Name')))}}
+                {{Form::text('name',null,array('class'=>'form-control font-style',
+                'disabled'=>'disabled','maxlength' => 35,'placeholder'=>__('Enter User Name')))}}
                 @error('name')
                 <small class="invalid-name" role="alert">
                     <strong class="text-danger">{{ $message }}</strong>
@@ -15,7 +16,8 @@
         <div class="col-md-6">
             <div class="form-group">
                 {{Form::label('email',__('Email'),['class'=>'form-label'])}}
-                {{Form::email('email',null,array('class'=>'form-control','disabled'=>'disabled','placeholder'=>__('Enter User Email')))}}
+                {{Form::email('email',null,array('class'=>'form-control',
+                'disabled'=>'disabled','placeholder'=>__('Enter User Email')))}}
                 @error('email')
                 <small class="invalid-email" role="alert">
                     <strong class="text-danger">{{ $message }}</strong>
