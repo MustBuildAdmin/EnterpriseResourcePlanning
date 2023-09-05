281e532c84618064195ce4b94187e8bb152ee74e
MustBuildAdmin <admin@mustbuildapp.com>
2023-09-05 20:14:14 +0530

    changes in developes

--- resources/views/new_layouts/header.blade.php
+++ resources/views/new_layouts/header.blade.php
@@ -211,7 +211,7 @@
                       <div class="nav-item dropdown">
                         <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown" aria-label="Open user menu">
                          
-						<span class="avatar avatar-sm" style="background-image: url({{(!empty(\Auth::user()->avatar))? $profile.\Auth::user()->avatar: asset(Storage::url("uploads/avatar/avatar.png"))}})"></span>
+						<span class="avatar avatar-sm" style="background: 'url({{(!empty(\Auth::user()->avatar))? $profile.\Auth::user()->avatar: asset(Storage::url("uploads/avatar/avatar.png"))}}')"></span>
                           <div class="d-none d-xl-block ps-2">
                             <div>{{\Auth::user()->name }}</div>
                             <div class="mt-1 small text-secondary">Role</div>
