ab00db8c41925d98d8dc2d41ac0ad06523afdd02
VijayMust <vijay018>
2023-09-12 17:55:26 +0530

    search

--- resources/views/consultants/index.blade.php
+++ resources/views/consultants/index.blade.php
@@ -176,19 +176,23 @@ body {
 				<div class="col-5">
 				   <div class="mb-3">
 					  <div class="row g-2">
-						 <div class="col">
-							<input type="text" class="form-control" placeholder="Search for…">
-						 </div>
+						<form action="{{ route('consultants.index') }}" method="GET">
+							<div class="input-group">
+								{{ Form::text('search',isset($_GET['search'])?$_GET['search']:'',
+								array('class' => 'form-control d-inline-block w-9 me-3 mt-auto',
+								'id'=>'search','placeholder'=>__('Search by Name')))
+								}}
 						 <div class="col-auto">
-							<a href="#" class="btn btn-icon" aria-label="Button">
-							   <!-- Download SVG icon from http://tabler-icons.io/i/search -->
+							<button   type="submit"  class="btn btn-icon" aria-label="Button">
 							   <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
 								  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
 								  <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path>
 								  <path d="M21 21l-6 -6"></path>
 							   </svg>
-							</a>
+							</button>
 						 </div>
+						</div>
+						</form>
 					  </div>
 				   </div>
 				</div>
