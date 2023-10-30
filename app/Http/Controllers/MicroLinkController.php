74883dc23a120ebc63bb956620987cfd24d33ee0
VijayMust <vijay018>
2023-10-30 18:13:25 +0530

    C

--- app/Http/Controllers/MicroLinkController.php
+++ app/Http/Controllers/MicroLinkController.php
@@ -13,12 +13,12 @@ class MicroLinkController extends Controller
 {
     public function store(Request $request)
     {
-        $max_id = MicroLink::where(['project_id' => Session::get('project_id')])->max('task_id');
+        $max_id = MicroLink::where(['project_id' => Session::get('project_id')])->max('id');
         if ($max_id == null) {
             $max_id = 0;
         }
         $link = new MicroLink();
-        $link->task_id = $max_id + 1;
+        $link->id = $max_id + 1;
         $rowid = $max_id + 1;
         $link->type = $request->type;
         $link->source = $request->source;
