80364182080eb6196669f3308559dda17455a7e2
gomathymustbuild <gomathy@mustbuildapp.com>
2023-11-01 21:03:09 +0530

    Code Smell Fix

--- resources/views/project_holidays/create.blade.php
+++ resources/views/project_holidays/create.blade.php
@@ -2,6 +2,7 @@
 .form-check {
     margin: 8px 12px !important;
 }
+
 #project_id{
     display:none;
 }
@@ -23,7 +24,7 @@
                     <div class="input-icon">
                         <span class="input-icon-addon">
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon"
-                             width="24" height="24" viewBox="0 0 24 24" 
+                             width="24" height="24" viewBox="0 0 24 24"
                             stroke-width="2" stroke="currentColor" fill="none"
                              stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
