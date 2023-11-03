d8340b8c4751d25e3543934c2964d06062783423
MustBuildAdmin <109482580+MustBuildAdmin@users.noreply.github.com>
2023-11-03 20:53:07 +0000

    dashboard changes

--- resources/views/construction_project/construction_dashboard.blade.php
+++ resources/views/construction_project/construction_dashboard.blade.php
@@ -314,16 +314,17 @@ if($delay>100){
                       <div class="col-auto">
                         <span
                           class="bg-twitter text-white avatar">
-                          <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
-                            viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                            stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path
-                              d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335
-                               -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395
-                                -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913
-                                 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0
-                                  0 .497 -3.753c0 -.249 1.51 -2.772 1.818 -4.013z" />
+                          <svg xmlns="http://www.w3.org/2000/svg" 
+                          class="icon icon-tabler icon-tabler-list-details" 
+                          width="24" height="24" viewBox="0 0 24 24" stroke-width="2" 
+                          stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M13 5h8"></path>
+                            <path d="M13 9h5"></path>
+                            <path d="M13 15h8"></path>
+                            <path d="M13 19h5"></path>
+                            <path d="M3 4m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z"></path>
+                            <path d="M3 14m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z"></path>
                           </svg>
                         </span>
                       </div>
