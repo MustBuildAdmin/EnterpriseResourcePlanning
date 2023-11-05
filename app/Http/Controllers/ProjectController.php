a1b121ac2c4da277a66c449c1b7d82d7f2a4c8ad
gomathymustbuild <125789194+gomathymustbuild@users.noreply.github.com>
2023-11-05 23:52:22 +0530

    Activity Table Server Side Rendering (#576)
    * unit testing bug fix
    
    * conflict fix
    
    * onsearch report
    
    * Code Smell Fix
    
    * Code Smell Fix
    
    * Code Smell Fix
    
    * Code Smell Fix
    
    * Code Smell Fix
    
    * Code Smell Fix
    
    * Code Smell Fixes
    
    * Code Smell Fixes
    
    * Code Smell Fixes
    
    * codesmell fix
    
    * Code Smell Fix
    
    * Invite Member UI, Contact API & Planning Search
    
    * Code Smell Fix
    
    * Code Smell Fix
    
    * VULNERABILITY Fix
    
    * Invite Team Member UI & Functionality
    
    * Code Smell Fix
    
    * Project Holidays UI & Functionality
    
    * Code Smell Fix
    
    * Code Smell Fix
    
    * Code Smell Fix
    
    * changes for code smell
    
    * Acitiviy UI & Table Change
    
    * Acitivity Table Server Side Rendering
    
    * Acitivity Table Server Side Rendering
    
    ---------
    
    Co-authored-by: gomagomathy <gomagomathy17223@gmail.com>
    Co-authored-by: gomathymustbuild <“gomathy@mustbuildapp.com”>
    Co-authored-by: MustBuildAdmin <109482580+MustBuildAdmin@users.noreply.github.com>

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -722,8 +722,34 @@ class ProjectController extends Controller
      * @param  \App\Poject  $poject
      * @return \Illuminate\Http\Response
      */
+    public function getActivityLog(Request $request, $project_id){
+      
+        // Page Length
+        $pageNumber = ( $request->start / $request->length )+1;
+        $pageLength = $request->length;
+        $skip       = ($pageNumber-1) * $pageLength;
+
+        // get data from products table
+        $query = \DB::table('activity_logs')->select('*')
+        ->join('users', 'users.id', '=', 'activity_logs.user_id');
+
+        // Search
+        $search = $request->search;
+        $query = $query->where(function($query) use ($search){
+            $query->orWhere('remark', 'like', "%".$search."%");
+            
+        });
+        $query->where("project_id", $project_id);
+
+        $recordsFiltered = $recordsTotal = $query->count();
+        $users = $query->skip($skip)->take($pageLength)->get();
+
+        return response()->json(["draw"=> $request->draw, "recordsTotal"=> $recordsTotal,
+         "recordsFiltered" => $recordsFiltered, 'data' => $users], 200);
+    }
     public function projectActivities(Request $request, $project_id)
     {
+
         $project = Project::where(["id" => $project_id])->first();
         $usr = Auth::user();
         if (\Auth::user()->type == "client") {
@@ -734,7 +760,7 @@ class ProjectController extends Controller
             $user_projects = $usr->projects->pluck("id")->toArray();
         }
         if (in_array($project->id, $user_projects)) {
-            return view("construction_project.activities", compact("project"));
+            return view("construction_project.activities", compact("project","project_id"));
         } else {
             return redirect()
                 ->back()
--- resources/views/construction_project/activities.blade.php
+++ resources/views/construction_project/activities.blade.php
@@ -39,7 +39,7 @@
                                     <div class="card-body">
                                        <div class="row">
                                         <div class="col-md-2 border-end p-3">
-                                        <form>
+                                        <form action="{{ route('project.activities', $project_id) }}">
                                             <div class="col-12 mb-3">
                                                 <label class="form-label required">Activity Start Date</label>
                                                 <div class="input-icon">
@@ -54,7 +54,8 @@
                                                         <path d="M16 3v4" /><path d="M8 3v4" /><path d="M4 11h16" />
                                                         <path d="M11 15h1" /><path d="M12 15v3" /></svg>
                                                     </span>
-                                                    <input class="form-control" placeholder="Select a Start date"
+                                                    <input class="form-control" name="start_date"
+                                                    placeholder="Select a Start date"
                                                      id="start-date"/>
                                                 </div>
                                             </div>
@@ -73,14 +74,14 @@
                                                     <path d="M11 15h1" /><path d="M12 15v3" /></svg>
                                                 </span>
                                                 <input class="form-control" placeholder="Select a End date"
-                                                 id="end-date"/>
+                                                name="end_date" id="end-date"/>
                                               </div>
                                            </div>
                                            <div class="col-md-12">
                                            <div class="mb-3">
                                             <label class="form-label">Task Action Method</label>
                                             <select type="text" class="form-select" placeholder="Select Status"
-                                             id="task-status" value="" multiple>
+                                             id="task-status" name="task_status" value="" multiple>
                                               <option value="Create">Create</option>
                                               <option value="Update">Update</option>
                                               <option value="Delete">Delete</option>
@@ -107,17 +108,6 @@
                                                       <th>Activity Date and Time</th>
                                                     </tr>
                                                   </thead>
-                                                  <tbody>
-                                                    @foreach($project->activities as $activity)
-                                                        <tr>
-                                                            <td><a href="#">{{$activity->id}}</a></td>
-                                                            <td>{!! $activity->logType() !!}</td>
-                                                            <td>{!! $activity->getRemark() !!}</td>
-                                                            <td>{!! $activity->doneBy() !!}</td>
-                                                            <td>{{$activity->created_at->diffForHumans()}}</td>
-                                                       </tr>
-                                                       @endforeach
-                                                  </tbody>
                                                 </table>
                                             </div>
                                         </div>
@@ -139,12 +129,164 @@
 <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js'
  integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT" crossorigin="anonymous"></script>
 
+      <script>
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+        $(document).ready(function() {
+            $('#task-table').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: {
+                    url: "{{ route('project.activitieslog', $project_id) }}",
+                    type: "POST",
+                    data: function (data) {
+                        data.search = $('input[type="search"]').val();
+                        data.start_date = $('#start-date').val();
+                        data.end_date = $('#end-date').val();
 
- <script>
-    new DataTable('#task-table', {
-      pagingType: 'full_numbers'
-  });
-      </script>
+                    }
+                },
+                order: ['1', 'DESC'],
+                pageLength: 10,
+                searching: false,
+                aoColumns: [
+                    {
+                        data: 'id',
+                    },
+                    {
+                        data: 'log_type',
+                        render: function(data, type, row) {
+                            let log_type=data;
+                            if (data == 'Added New Task') {
+                                log_type = "{{__('Create')}}";
+                            } else if (data == 'Updated Task') {
+                                log_type = "{{__('Update')}}";
+                            } else if (data == 'Deleted Task') {
+                                log_type = "{{__('Delete')}}";
+                            }
+                            return log_type;
+                        }
+                    },
+                    {
+                        data: 'remark',
+                        render: function(data, type, row) {
+                            data=JSON.parse(data);
+                            console.log(data);
+                            if (row['log_type'] == 'Invite User') {
+                                return "{{__('has invited')}}"+' - <b>'+data['title']+'</b>';
+                            }
+                            else if (row['log_type'] == 'Added New Task') {
+                                return "{{__('Added New Task')}}"+' - <b>'+data['title']+'</b>';
+                            }
+                            else if (row['log_type'] == 'User Assigned to the Task') {
+                                return "{{__('has assigned task ')}}"+' - <b>'+data['task_name']
+                                +'</b> '+"{{__(' to')}}"+
+                                ' - <b>'+data['member_name']+'</b>';
+                            }else if (row['log_type'] == 'User Removed from the Task') {
+                                return "{{__('has removed ')}}"+' - <b>'+data['member_name']
+                                +'</b>'+"{{__(' from task')}}"+
+                                ' - <b>'+data['task_name']+'</b>';
+                            } else if (row['log_type'] == 'Upload File') {
+                                return "{{__('Upload new file')}}"+' - <b>'+data['file_name']+'</b>';
+                            } else if (row['log_type'] == 'Create Bug') {
+                                return "{{__('Created new bug')}}"+' - <b>'+data['title']+'</b>';
+                            } else if (row['log_type'] == 'Create Milestone') {
+                                return "{{__('Create new milestone')}}"+' - <b>'+data['title']+'</b>';
+                            } else if (row['log_type'] == 'Create Task') {
+                                return "{{__('Create new Task')}}"+' - <b>'+data['title']+'</b>';
+                            } else if (row['log_type'] == 'Create Expense') {
+                                return "{{__('Create new Expense')}}"+' - <b>'+data['title']+'</b>';
+                            } else if (row['log_type'] == 'Add Product') {
+                                return "{{__('Add new Products')}}"+' - <b>'+data['title']+'</b>';
+                            } else if (row['log_type'] == 'Update Sources') {
+                                return "{{__('Update Sources')}}";
+                            } else if (row['log_type'] == 'Create Deal Call') {
+                                return "{{__('Create new Deal Call')}}";
+                            } else if (row['log_type'] == 'Create Deal Email') {
+                                return "{{__('Create new Deal Email')}}";
+                            }else if (row['log_type'] == 'Added New Consultant') {
+                                return "{{__('Added new Consultant')}}"+' - <b>'+data['title']+'</b>';
+                            } else if (row['log_type'] == 'Updated Consultant') {
+                                return "{{__('Updated Consultant')}}"+' - <b>'+data['title']+'</b>';
+                            } else if (row['log_type'] == 'Deleted Consultant') {
+                                return "{{__('Deleted Consultant')}}"+' - <b>'+data['title']+'</b>';
+                            } else if (row['log_type'] == 'Added New RFIStatus') {
+                                return "{{__('Added New RFIStatus')}}"+' - <b>'+data['title']+'</b>';
+                            } else if (row['log_type'] == 'Updated RFIStatus') {
+                                return "{{__('Updated RFIStatus')}}"+' - <b>'+data['title']+'</b>';
+                            } else if (row['log_type'] == 'Deleted RFIStatus') {
+                                return "{{__('Deleted RFIStatus')}}"+' - <b>'+data['title']+'</b>';
+                            } else if (row['log_type'] == 'Added New ProjectSpecification') {
+                                return "{{__('Added New ProjectSpecification')}}"+' - <b>'+data['title']+'</b>';
+                            } else if (row['log_type'] == 'Updated ProjectSpecification') {
+                                return "{{__('Updated ProjectSpecification')}}"+' - <b>'+data['title']+'</b>';
+                            } else if (row['log_type'] == 'Added New Variation Scope') {
+                                return "{{__('Added New Variation Scope')}}"+' - <b>'+data['title']+'</b>';
+                            } else if (row['log_type'] == 'Updated Variation Scope') {
+                                return "{{__('Updated Variation Scope')}}"+' - <b>'+data['title']+'</b>';
+                            } else if (row['log_type'] == 'Deleted Variation Scope') {
+                                return "{{__('Deleted Variation Scope')}}"+' - <b>'+data['title']+'</b>';
+                            } else if (row['log_type'] == 'Added New ProcurementMaterial') {
+                                return "{{__('Added New ProcurementMaterial')}}"+' - <b>'+data['title']+'</b>';
+                            } else if (row['log_type'] == 'Updated ProcurementMaterial') {
+                                return "{{__('Updated ProcurementMaterial')}}"+' - <b>'+data['title']+'</b>';
+                            } else if (row['log_type'] == 'Deleted ProcurementMaterial') {
+                                return "{{__('Deleted ProcurementMaterial')}}"+' - <b>'+data['title']+'</b>';
+                            } else if (row['log_type'] == 'Added New SiteReport') {
+                                return "{{__('Added New SiteReport')}}"+' - <b>'+data['title']+'</b>';
+                            } else if (row['log_type'] == 'Updated SiteReport') {
+                                return "{{__('Updated SiteReport')}}"+' - <b>'+data['title']+'</b>';
+                            } else if (row['log_type'] == 'Deleted SiteReport') {
+                                return "{{__('Deleted SiteReport')}}"+' - <b>'+data['title']+'</b>';
+                            } else if (row['log_type'] == 'Added New ConcretePouring') {
+                                return "{{__('Added New ConcretePouring')}}"+' - <b>'+data['title']+'</b>';
+                            } else if (row['log_type'] == 'Updated ConcretePouring') {
+                                return "{{__('Updated ConcretePouring')}}"+' - <b>'+data['title']+'</b>';
+                            } else if (row['log_type'] == 'Deleted ConcretePouring') {
+                                return "{{__('Deleted ConcretePouring')}}"+' - <b>'+data['title']+'</b>';
+                            } else if (row['log_type'] == 'Updated Task') {
+                                return "{{__('Updated Task')}}"+' - <b>'+data['title']+'</b>';
+                            } else if (row['log_type'] == 'Deleted Task') {
+                                return "{{__('Deleted Task')}}"+' - <b>'+data['title']+'</b>';
+                            }else if (row['log_type'] == 'Move Task') {
+                                return "{{__('Moved the Task')}}"+' - <b>'+data['title']+'</b> '+"{{__('from')}}"+' '+
+                                data['old_stage'].toUpperCase()+' '+"{{__('to')}}"+' '+data['new_stage'].toUpperCase();
+                            } else if (row['log_type'] == 'Move') {
+                                return "{{__('Moved the deal')}}"+' - <b>'+data['title']+'</b> '+"{{__('from')}}"+' '
+                                + data['old_status'].toUpperCase()+' '+"{{__('to')}}"+' '
+                                +data['new_status'].toUpperCase();
+                            } else {
+                                return data.toUpperCase();
+                            }
+                        
+
+                        }
+                    },
+                    {
+                        data: 'user_id',
+                        render: function(data, type, row) {
+                          return capitalize(row['name']);
+                        }
+                    },
+                    {
+                        data: 'created_at',
+                        render: function(data, type, row) {
+                          return new Date(data).toDateString()+" "+new Date(data).toLocaleTimeString();
+                        }
+                    }
+                ]
+            });
+        });
+
+        function capitalize(s)
+        {
+            return s[0].toUpperCase() + s.slice(1);
+        }
+
+    </script>
 <script>
     // @formatter:off
     document.addEventListener("DOMContentLoaded", function () {
