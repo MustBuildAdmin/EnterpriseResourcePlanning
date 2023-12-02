75179c2bdf289950c548b99c7a1c718102c856b5
MustBuildAdmin <admin@mustbuildapp.com>
2023-12-02 19:02:10 +0530

    changes in the modal

--- resources/views/construction_project/instance_view.blade.php
+++ resources/views/construction_project/instance_view.blade.php
@@ -3,7 +3,7 @@
         <div class="row">
             @php $row=0; @endphp
             @foreach ($get_project_instances as $key => $project)
-                
+
                 <div class="col-md-6 col-xxl-6 divstyle">
                     <div class="card">
                         <div class="card-body">
@@ -11,21 +11,26 @@
                                 <div class="d-flex align-items-center">
                                     @if($key==0)
                                          @php  $name='Main'; @endphp
-                                        <h5 class="mb-0"><a class="text-dark" href="{{ route('projects.instance_project', [$project->id,$project->project_id,$name]) }}">Main</a></h5>
+                                        <h5 class="mb-0"><a class="text-dark"
+                                        href="{{ route('projects.instance_project',
+                                        [$project->id,$project->project_id,$name]) }}">Main</a></h5>
                                     @else
-                                        @php $row=$row+1; 
+                                        @php $row=$row+1;
                                         $name='Revision'.$row; @endphp
-                                        <h5 class="mb-0"><a class="text-dark" href="{{ route('projects.instance_project', [$project->id,$project->project_id,$name]) }}">Revision {{$row}}</a></h5>
+                                        <h5 class="mb-0"><a class="text-dark"
+                                            href="{{ route('projects.instance_project',
+                                            [$project->id,$project->project_id,$name]) }}">Revision {{$row}}</a></h5>
                                     @endif
                                 </div>
                             </div>
                         </div>
-                        <div class="card mb-0 mt-3">
+                        <div class="card m-0">
                             <div class="card-body p-3">
                                 <div class="row">
                                     <div class="col-6">
                                         <h6
-                                            class="mb-0 {{ strtotime($project->start_date) < time() ? 'text-danger' : '' }}">
+                                            class="mb-0
+                                            {{ strtotime($project->start_date) < time() ? 'text-danger' : '' }}">
                                             {{ Utility::getDateFormated($project->start_date) }}</h6>
                                         <p class="text-muted text-sm mb-0">{{ __('Start Date') }}</p>
                                     </div>
@@ -42,4 +47,3 @@
             @endforeach
         </div>
     </div>
-    
