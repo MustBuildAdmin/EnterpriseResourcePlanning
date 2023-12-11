9b7d82374abbd9c25f70a93110ea1868fcbd1c82
Gomathy <gomathy@mustbuildapp.com>
2023-12-11 13:00:55 +0530

    Phone number feature on create client

--- resources/views/new_layouts/header.blade.php
+++ resources/views/new_layouts/header.blade.php
@@ -78,6 +78,12 @@
 
     <script src="{{ asset('assets/dist/js/chosen.jquery.js') }}"></script>
     <style>
+      .iti.iti--allow-dropdown.iti--separate-dial-code.iti--show-flags{
+         width:100%;
+      }
+      .country_code{
+         margin-top:5px !important;
+      }
         @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
         :root {
