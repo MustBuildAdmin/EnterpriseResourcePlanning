9ce65ab87753cb4ee8a40dcc640903a5ca194182
VijayMust <vijay018>
2023-12-28 11:10:33 +0530

    change in code

--- resources/views/new_layouts/header.blade.php
+++ resources/views/new_layouts/header.blade.php
@@ -11,7 +11,7 @@
     $color = !empty($setting['color']) ? $setting['color'] : 'theme-3';
     $profile = \App\Models\Utility::get_file('uploads/avatar/');
     $SITE_RTL = Utility::getValByName('SITE_RTL');
-
+    $userlogo=\App\Models\Utility::get_file('uploads/logo/');
 @endphp
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="{{ $SITE_RTL == 'on' ? 'rtl' : '' }}">
@@ -317,7 +317,7 @@
                      <span class="avatar avatar-sm"
                         style="background-image: url('{{ !empty(\Auth::user()->avatar) ?
                         $profile . \Auth::user()->avatar :
-                        asset(Storage::url('uploads/avatar/avatar.png')) }}')"></span>
+                        $userlogo.Config::get('constants.AVATAR') }}')"></span>
                      <div class="d-none d-xl-block ps-2">
                         <div>{{ \Auth::user()->name }}</div>
                         @if(\Auth::user()->type=='company')
